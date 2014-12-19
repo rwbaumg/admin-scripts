04371c27529fc1db969e4278a9d9d06a04a2aa98
Robert W. Baumgartner <rwb@0x19e.net>
2014-12-18 18:18:15 -0600

    added cleanup-boot.sh script (for cleaning unused kernels)

---
+++ cleanup-boot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Clean up the /boot partition
+dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
