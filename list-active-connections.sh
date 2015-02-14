63b921f28977b9d226b265eba809297df9cd0bc0
Robert W. Baumgartner <rwb@0x19e.net>
2015-02-14 12:20:40 -0600

    added list-active-connections.sh script for listing active connections using netstat

---
+++ list-active-connections.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+netstat -an | awk '{print $5}' | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | egrep -v "(`for i in \`ip addr | grep inet |grep eth0 | cut -d/ -f1 | awk '{print $2}'\`;do echo -n "$i|"| sed 's/\./\\\./g;';done`127\.|0\.0\.0)" | sort -n | uniq -c | sort -n
