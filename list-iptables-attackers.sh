48c8e0a288cda26b3f3c862cfe2978cbabdeeb2b
Robert W. Baumgartner <rwb@0x19e.net>
2015-02-14 12:57:42 -0600

    fixed formatting in list-iptables-attackers.sh output

--- list-iptables-attackers.sh
+++ list-iptables-attackers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-grep "iptables dropped:" /var/log/syslog | awk  -F" " '$22 ~ /^DPT/{printf("%s \t %s/%s \n", $13, $22, $20);next}{printf("%s \t %s/%s \n", $13, $23, $21)}' | sed s/SRC=// | sed s/PROTO=// | sed s/DPT=// | sort -n | uniq -c | sort -rn | head -n 10
+grep "iptables dropped:" /var/log/syslog | awk  -F" " '$22 ~ /^DPT/{printf("%-15s\t%s/%s\n", $13, $22, $20);next}{printf("%-15s\t%s/%s\n", $13, $23, $21)}' | sed s/SRC=// | sed s/PROTO=// | sed s/DPT=// | sort -n | uniq -c | sort -rn | head -n 10
 # grep "iptables dropped:" /var/log/syslog* | awk  -F" " '$22 ~ /^DPT/{printf("%s \t %s/%s \n", $13, $22, $20);next}{printf("%s \t %s/%s \n", $13, $23, $21)}' | sed s/SRC=// | sed s/PROTO=// | sed s/DPT=// | sort -n | uniq -c | sort -rn | head -n 10
