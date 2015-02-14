16647f2833736a5da2ce5f594c7e88a4bbb025a2
Robert W. Baumgartner <rwb@0x19e.net>
2015-02-14 15:20:03 -0600

    added dump-info.sh

---
+++ dump-info.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# get some info about the current host in a nifty one-liner
+# rwb[at]0x19e.net
+
+(date;hostname --fqdn;id;who -apb;uptime;uname -a;df -hT;cat /proc/cpuinfo|grep "model name"|uniq -c;cat /proc/meminfo|grep MemTotal|uniq -c;ifconfig|grep 'inet addr';echo;wget --output-document=/dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip 2>&1 >/dev/null|grep --before-context=5 saved;echo;finger root;lsof -i)|uniq
