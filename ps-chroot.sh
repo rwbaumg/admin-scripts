bd8ff9b5f61705ffe7f4362fb9d686b986e8a9b6
Robert W. Baumgartner <rwb@0x19e.net>
2015-02-14 16:23:39 -0600

    ps-chroot: fixed to actually work again (look for mountpoint instead of chroot path

--- ps-chroot.sh
+++ ps-chroot.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-CHROOT_ROOT="/srv/chroot/"
-PROCS=""
+# list processes running inside of schroot jails
+# this script will enumerate all of the installed 
+# schroots, and then check the mount point to see
+# if it's jailed.
+
+# the mountpoint used by schroot
+# this is used to determine which procs are jailed so it must be correct!
+MOUNTPOINT="/var/lib/schroot/mount"
+
 for chroot in `schroot -l|awk -F : '{print $2}'`; do
+  PROCS=""
   for p in `ps -o pid -A`; do
-    if [ "`readlink /proc/$p/root`" = "$CHROOT_ROOT/$chroot" ]; then
+    LINK=$(readlink /proc/$p/root)
+    if [[ $LINK == "$MOUNTPOINT/$chroot"* ]]; then
       PROCS="$PROCS $p"
     fi
   done
