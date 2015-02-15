40f758dd3818f042f40423b62ab18215434aa746
Robert W. Baumgartner <rwb@0x19e.net>
2015-02-15 02:36:36 -0600

    list-iptables-attackers: cleanup

--- list-iptables-attackers.sh
+++ list-iptables-attackers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-LOGPATTERN=/var/log/syslog*
+LOGPATTERN="/var/log/syslog*"
 grep "iptables dropped:" $LOGPATTERN | awk  -F" " '$22 ~ /^DPT/{printf("%-15s\t%s/%s\n", $13, $22, $20);next}{printf("%-15s\t%s/%s\n", $13, $23, $21)}' | sed s/SRC=// | sed s/PROTO=// | sed s/DPT=// | sort -n | uniq -c | sort -rn | head -n 10
