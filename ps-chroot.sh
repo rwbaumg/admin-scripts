1eebb79720a12c1de678fa3f01c1361910139e22
Robert W. Baumgartner <rwb@0x19e.net>
2015-02-16 00:21:50 -0600

    ps-chroot: use sudo when calling readlink (otherwise it will silently fail)

--- ps-chroot.sh
+++ ps-chroot.sh
@@ -11,7 +11,7 @@ MOUNTPOINTS=("/var/lib/schroot/mount" "/var/chroot")
 for chroot in `schroot -l|awk -F : '{print $2}'`; do
   PROCS=""
   for p in `ps -o pid -A`; do
-    LINK=$(readlink /proc/$p/root)
+    LINK=$(sudo readlink /proc/$p/root)
     for mp in "${MOUNTPOINTS[@]}"; do
       if [[ $LINK == "$mp/$chroot"* ]]; then
         PROCS="$PROCS $p"
