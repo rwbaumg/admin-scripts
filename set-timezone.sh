b5f43704c6b25e8ab30ffecdfe041d3e71c0377a
Robert W. Baumgartner <rwb@0x19e.net>
2015-02-15 23:09:09 -0600

    added script for configuring system timezone

---
+++ set-timezone.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ZONE_TAB="/usr/share/zoneinfo/zone.tab"
+
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 <tz_name>"
+  exit 1
+fi
+
+TZ_NAME=$(cat $ZONE_TAB | grep -i "$1" | awk '{print $3}')
+if [[ -z "$TZ_NAME" ]]; then
+  echo "ERROR: Failed to find timezone for '$1'"
+  exit 1
+fi
+
+echo $TZ_NAME | sudo tee /etc/timezone
+sudo dpkg-reconfigure --frontend noninteractive tzdata
