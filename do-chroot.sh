e527c2540f67e7f9339dd471a6aba51a67b783ee
Robert W. Baumgartner <rwb@0x19e.net>
2015-02-22 12:46:16 -0600

    do-chroot: updated to work with latest schroot (now uses or creates schroot session to run commands in)

--- do-chroot.sh
+++ do-chroot.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
-args=""
-for i in "$@" ; do
-    args="$args '$i'"
-    # echo $args
+# runs a command in the specified schroot
+# rwb [ rwb[at]0x19e.net ]
+
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 <schroot> [COMMAND]"
+  exit 1
+fi
+
+# The full path to the schroot binary
+SCHROOT_BIN="/usr/bin/schroot"
+
+# The name of the schroot to jail the daemon in
+SCHROOT_NAME="$1"
+
+# Get the command to run
+CMD_ARGS="${*:2}"
+
+echo -e "schroot \t : $SCHROOT_NAME"
+echo -e "command \t : $CMD_ARGS"
+
+# check to make sure the specified schroot actually exists
+CHROOT_EXISTS=""
+for ch in `$SCHROOT_BIN -l -a|grep "chroot"|awk -F: '{print $2}'`; do
+  if [[ $ch == "$SCHROOT_NAME"* ]]; then
+    CHROOT_EXISTS="y"
+  fi
 done
+if [[ -z "$CHROOT_EXISTS" ]]; then
+  echo "ERROR: The specified chroot, '$SCHROOT_NAME', does not exist."
+  exit 1
+fi
 
-echo chroot  : $1
-echo command : $2
-echo args    : $3
-#echo args    : $args
+# Get the schroot session (or create a new one)
+SESSION=""
+for sn in `$SCHROOT_BIN -l -a|grep "session:"|awk -F: '{print $2}'`; do
+  if [[ $sn == "$SCHROOT_NAME"* ]]; then
+    SESSION=$sn
+  fi
+done
+if [[ -z "$SESSION" ]]; then
+  echo "Creating new schroot session for '$SCHROOT_NAME' ..."
+  SESSION=$(schroot --begin-session -c $SCHROOT_NAME)
+fi
+echo "Using schroot session '$SESSION' ..."
 
-/usr/bin/schroot -c $1 --directory=/root "$2" $3
-#/usr/bin/schroot -c $1 --directory=/root "$2" $args
+echo "Running command '$CMD_ARGS' ..."
+$SCHROOT_BIN --directory / --run-session -c "$SESSION" $CMD_ARGS
