5ff98ef257751844aa499aefa0300e65be9284e2
Robert W. Baumgartner <rwb@0x19e.net>
2015-02-25 22:43:13 -0600

    added cpu-usage.sh (for getting overall system CPU usage using either mpstat or top)

---
+++ cpu-usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# gets current (total) cpu usage
+# rwb < rwb[at]0x19e.net >
+
+MPSTAT_BIN=$(which mpstat)
+TOP_BIN=$(which top)
+
+if [[ -z "$MPSTAT_BIN" ]]; then
+  # echo "Using '$TOP_BIN' for calculating CPU usage..."
+
+  $TOP_BIN -bn1 | grep "Cpu(s)" | \
+             sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
+             awk '{print 100 - $1"%"}'
+else
+  # echo "Using '$MPSTAT_BIN' for calculating CPU usage..."
+  $MPSTAT_BIN | awk '$12 ~ /[0-9.]+/ { print 100 - $13"%" }'
+fi
