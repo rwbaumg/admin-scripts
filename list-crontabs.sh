41a21475c1aa9c8788f7ab07589b12e7769afda4
Robert W. Baumgartner <rwb@0x19e.net>
2015-05-02 19:02:35 -0500

    updated output formatting in list-crontabs.sh

--- list-crontabs.sh
+++ list-crontabs.sh
@@ -16,23 +16,23 @@ COL_BLUE=$ESC_SEQ"34;01m"
 COL_MAGENTA=$ESC_SEQ"35;01m"
 COL_CYAN=$ESC_SEQ"36;01m"
 
-printf "$COL_RED%.0s-$COL_RESET" {1..20}; echo
+printf "$COL_RED%.0s-$COL_RESET" {1..30}; echo
 for user in $(cut -f1 -d: /etc/passwd); do
   OUT=$( crontab -u $user -l 2>/dev/null )
   if [[ $OUT ]]; then
     echo -e  "$COL_GREEN crontab for $user $COL_RESET"
-    printf "$COL_RED%.0s-$COL_RESET" {1..20}; echo
-    echo "   minute (0-59),"
-    echo "   | hour (0-23),"
-    echo "   | | day of the month (1-31),"
-    echo "   | | | month of the year (1-12),"
-    echo "   | | | | day of the week (0-6 with 0=Sunday)."
-    echo "   | | | | | command(s)"
-    echo "   | | | | | |"
+    printf "$COL_RED%.0s-$COL_RESET" {1..30}; echo
+    echo -e "$COL_CYAN minute (0-59), $COL_RESET"
+    echo -e "$COL_CYAN |  hour (0-23), $COL_RESET"
+    echo -e "$COL_CYAN |  |  day of the month (1-31), $COL_RESET"
+    echo -e "$COL_CYAN |  |  |  month of the year (1-12), $COL_RESET"
+    echo -e "$COL_CYAN |  |  |  |  day of the week (0-6 with 0=Sunday). $COL_RESET"
+    echo -e "$COL_CYAN |  |  |  |  |  command(s) $COL_RESET"
+    echo -e "$COL_CYAN |  |  |  |  |  | $COL_RESET"
     echo "$OUT" | grep -v '^#' | grep -v '^$'
-    printf "$COL_RED%.0s-$COL_RESET" {1..20}; echo
+    printf "$COL_RED%.0s-$COL_RESET" {1..30}; echo
   fi
 done
 
 echo -e "$COL_YELLOW Finished listing crontabs$COL_RESET"
-printf "$COL_RED%.0s-$COL_RESET" {1..20}; echo
+printf "$COL_RED%.0s-$COL_RESET" {1..30}; echo
