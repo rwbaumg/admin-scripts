ec422e63f7147aae045cf692e9553006ff8fd383
slynx <slynx@0x19e.net>
2015-06-02 19:57:33 -0500

    add find-setuid script (for finding setuid binaries)

---
+++ find-setuid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# find all setuid binaries
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+find / -xdev \( -perm -4000 \) -type f -print0 | xargs -0 ls -l
