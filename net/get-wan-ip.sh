569d6a4dc6c93347c524d8456fa02fdca7308c73
slynx <slynx@0x19e.net>
2015-06-04 18:49:07 -0500

    cleanup get-wan-ip script

--- net/get-wan-ip.sh
+++ net/get-wan-ip.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 # Output current WAN IP
 
-# IGNORE_ADDRS=("127.0.0.1" "10.6.6")
-
 ifconfig | grep 'inet addr:' \
          | grep -v '127.0.0.1' \
          | cut -d: -f2 \
          | awk '{ print $1}'
-
-#ifconfig | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v '10.6.6' | cut -d: -f2 | awk '{ print $1}'
-
-# ifconfig fwbr.wan| grep 'inet addr' | awk '{print $2}' | cut -f 2 -d ':'
