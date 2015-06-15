7d5f638e816ea31d398f7fefbd2d2e277e98f961
Robert W. Baumgartner <rwb@0x19e.net>
2015-06-15 12:18:29 -0500

    update ls-chmod to support taking an argument

--- ls-chmod.sh
+++ ls-chmod.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# list file and folder permissions
 
+if [[ -z "$1" ]]; then
 ls -l | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/) \
             *2^(8-i));if(k)printf("%0o ",k);print}'
+else
+ls -l "$1" | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/) \
+                 *2^(8-i));if(k)printf("%0o ",k);print}'
+fi
