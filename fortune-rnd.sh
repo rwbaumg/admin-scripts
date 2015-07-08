db588d19e71d2e20838302fc4b7153dabc3a58a7
Robert W. Baumgartner <rwb@0x19e.net>
2015-07-07 19:32:20 -0500

    remove static path to games from fortune-rnd.sh

--- fortune-rnd.sh
+++ fortune-rnd.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-GAMES_DIR=/usr/games
-
 # check if curl command exists
 hash fortune 2>/dev/null || { echo >&2 "You need to install fortune. Aborting."; exit 1; }
 hash cowsay 2>/dev/null || { echo >&2 "You need to install cowsay. Aborting."; exit 1; }
@@ -13,9 +11,9 @@ fi
 
 cow_file_length=`ls -1 $COWPATH | wc -l`
 
-RANDOM=$$ # initialized the random seed with the process id of this script
+# initialize the random seed with the process id of this script
+RANDOM=$$
 let "random_line = $RANDOM % $cow_file_length + 1"
 cow=`ls -1 $COWPATH | head -n $random_line | tail -n 1`
 
-$GAMES_DIR/fortune | $GAMES_DIR/cowsay -n -f $cow
-
+fortune | cowsay -n -f $cow
