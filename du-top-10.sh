3bb4efbb7f2143902a0ef0a2bff9cc77db69d3f0
root <root@athena.0x19e.net>
2015-07-25 21:44:38 -0500

    update du-top-10.sh script to accept an argument

--- du-top-10.sh
+++ du-top-10.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # find large files
 
-du -hsx * | sort -rh | head -10
+TOP_10_MSG="top 10 by size"
+if [ -n "$1" ]; then
+  echo "$TOP_10_MSG: $1"
+else
+  echo "$TOP_10_MSG"
+fi
+
+du -hsx $1* \
+  | sort -rh \
+  | head -10
