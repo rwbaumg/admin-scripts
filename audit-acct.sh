1baf57f72006269a67c63359620b4382df086650
Robert W. Baumgartner <rwb@0x19e.net>
2015-08-03 22:33:18 -0500

    add autit-acct.sh script

---
+++ audit-acct.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+hash ac 2>/dev/null || { echo >&2 "You need to install acct. Aborting."; exit 1; }
+
+echo "USERS' CONNECT TIMES"
+echo ""
+
+ac -d -p
+
+echo ""
+echo "COMMANDS BY USER"
+echo ""
+
+users=$(cat /etc/passwd | awk -F ':' '{print $1}' | sort)
+
+for user in $users ; do
+  comm=$(lastcomm --user $user | awk '{print $1}' | sort | uniq -c | sort -nr)
+  if [ "$comm" ] ; then
+    echo "$user:"
+    echo "$comm"
+  fi
+done
+
+echo ""
+echo "COMMANDS BY FREQUENCY OF EXECUTION"
+echo ""
+
+sa | awk '{print $1, $6}' | sort -n | head -n -1 | sort -nr
