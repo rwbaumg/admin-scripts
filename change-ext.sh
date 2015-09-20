1e0e968672a8e73bd4f85bee661d3b524d98a461
slynx <slynx@0x19e.net>
2015-09-20 14:46:48 -0500

    add change-ext.sh (for changing file extensions)

---
+++ change-ext.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# script to change file extensions
+
+if [ "$#" -ne 3 ]; then
+  echo >&2 "Usage: $0 <path> <ext> <new_ext>"
+fi
+
+echo "Updating extensions in $1 ..."
+echo "Converting $2 -> $3"
+
+for f in *.$2; do
+  mv -v "$f" "$(basename "$f" .$2).$3"
+done
