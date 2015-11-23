5aff1323a90c6d998adb418291bd2153aa6d491a
Robert W. Baumgartner <rwb@0x19e.net>
2015-11-23 01:11:04 -0600

    git-svn-setignored: add check for git-svn command

--- git-svn-setignored.sh
+++ git-svn-setignored.sh
@@ -3,6 +3,11 @@
 
 hash git 2>/dev/null || { echo >&2 "You need to install git. Aborting."; exit 1; }
 
+if ! git svn --help > /dev/null 2>&1; then
+  echo >&2 "git-svn does not appear to be installed. Aborting."
+  exit 1
+fi
+
 if ! git rev-parse --git-dir > /dev/null 2>&1; then
   echo >&2 "Current directory is not a git repository. Aborting."
   exit 1
