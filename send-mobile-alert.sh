ece1d903d4a189de25e6c9e28d03e4a81741daf9
Robert W. Baumgartner <rwb@0x19e.net>
2015-11-24 22:30:27 -0600

    major cleanup to send-mobile-alert script

--- send-mobile-alert.sh
+++ send-mobile-alert.sh
@@ -2,11 +2,6 @@
 # script to send an alert text
 # rwb [ rwb[at]0x19e.net ]
 
-if [[ -z "$1" ]]; then
-  echo "Usage: $0 [opts]"
-  exit 1
-fi
-
 # Path to FFMpeg binary
 FFMPEG_BIN="/usr/bin/ffmpeg"
 
@@ -17,12 +12,15 @@ MOBILE=""
 NOW=$(date +"%m-%d-%Y %r")
 ATTACHMENT=""
 FORCE_MMS=""
+MAIL_ARGS=""
+VERBOSE_SWITCH=""
+VERBOSITY=0
 
 # The SMTP server to use for sending e-mails (must have STARTTLS turned on)
 SMTP_SERVER="mail.0x19e.net:587"
 
 # The gateway (last part of the e-mail address) used for sending mobile text messages.
-# This is the preferred gateway when no image attachment is specified, and is usually 
+# This is the preferred gateway when no image attachment is specified, and is usually
 # faster than sending a message via an MMS gateway.
 TXT_GATEWAY="txt.att.net"
 
@@ -44,62 +42,159 @@ SUBJECT="SECURITY ALERT"
 # $MOBILE     The mobile phone number the alert is being sent to.
 MESSAGE="A security related event was detected on $NOW \n Action may be required."
 
+exit_script()
+{
+    # Default exit code is 1
+    local exit_code=1
+    local re var
+
+    re='^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$'
+    if echo "$1" | egrep -q "$re"; then
+        exit_code=$1
+        shift
+    fi
+
+    re='[[:alnum:]]'
+    if echo "$@" | egrep -iq "$re"; then
+        echo
+        if [ $exit_code -eq 0 ]; then
+            echo "INFO: $@"
+        else
+            echo "ERROR: $@" 1>&2
+        fi
+    fi
+
+    # Print 'aborting' string if exit code is not 0
+    [ $exit_code -ne 0 ] && echo "Aborting script..."
+
+    exit $exit_code
+}
+
+usage()
+{
+    # Prints out usage and exit.
+    sed -e "s/^    //" -e "s|SCRIPT_NAME|$(basename $0)|" \
+                       -e "s|MMS_GATEWAY|$MMS_GATEWAY|" \
+                       -e "s|TXT_GATEWAY|$TXT_GATEWAY|" \
+                       -e "s|SMTP_SERVER|$SMTP_SERVER|" \
+                       -e "s|MAIL_FROM|$MAIL_FROM|" \
+                       -e "s|SUBJECT|$SUBJECT|" \
+        <<"    EOF"
+    USAGE
+
+    Send a message to a mobile device.
+
+    SYNTAX
+            SCRIPT_NAME [OPTIONS]
+
+    OPTIONS
+
+     -n, --mobile-number <value>   The mobile number to send an alert to.
+     -f, --mail-from <value>       Sets the sender property for the message. (default: MAIL_FROM)
+     -s, --subject <value>         The message subject. (default: SUBJECT)
+     -m, --message <value>         The message body.
+
+     -a, --attachment <value>      Adds a media attachement to the message. Videos
+                                   will be compressed to the required size.
+
+     --mms-gateway <value>         Sets the MMS gateway to use for sending MMS messages. (default: MMS_GATEWAY)
+     --txt-gateway <value>         Sets the TXT gateway to use for sending text messages. (default: TXT_GATEWAY)
+     --smtp-server <value>         Sets the SMTP server for sending e-mail alerts. (default: SMTP_SERVER)
+     --force-mms                   Send the message using MMS, event if it only contains text.
+
+     -v, --verbose                 Make the script more verbose. This option can be specified multiple times.
+     -h, --help                    Prints this usage.
+    EOF
+
+    exit_script $@
+}
+
+test_arg()
+{
+    # Used to validate user input
+    local arg="$1"
+    local argv="$2"
+
+    if [ -z "$argv" ]; then
+        if echo "$arg" | egrep -q '^-'; then
+            usage "Null argument supplied for option $arg"
+        fi
+    fi
+
+    if echo "$argv" | egrep -q '^-'; then
+        usage "Argument for option $arg cannot start with '-'"
+    fi
+}
+
+# verify required programs
+hash mailx 2>/dev/null || { echo >&2 "You need to install mailx. Aborting."; exit 1; }
+
+# process arguments
+[ $# -gt 0 ] || usage
 while [ $# -gt 0 ]; do
 case "$1" in
   -n|--mobile-number)
-    MOBILE="$2"
+    test_arg "$1" "$2"
+    shift
+    MOBILE="$1"
     echo "Mobile number set to: $MOBILE"
     shift
   ;;
   -s|--subject)
-    SUBJECT="$2"
+    test_arg "$1" "$2"
+    shift
+    SUBJECT="$1"
     echo "Subject set to: $SUBJECT"
     shift
   ;;
   -m|--message)
-    if [[ -z "$2" ]]; then
-      echo "You must supply a message along with the -m|--message option."
-      exit 1
-    fi
-    MESSAGE=$(echo -e "$2")
+    test_arg "$1" "$2"
+    shift
+    MESSAGE=$(echo -e "$1")
     echo "Message set to: $MESSAGE"
     shift
   ;;
   -f|--mail-from)
-    MAIL_FROM="$2"
+    test_arg "$1" "$2"
+    shift
+    MAIL_FROM="$1"
     echo "MAIL_FROM set to: $MAIL_FROM"
     shift
   ;;
   -a|--attachment)
-    if [[ ! -z "$2" ]]; then
-      if [ ! -f "$2" ]; then
-        echo "Specified file not found!"
+    test_arg "$1" "$2"
+    shift
+    if [[ ! -z "$1" ]]; then
+      if [ ! -f "$1" ]; then
+        echo >&2 "Specified file not found!"
         exit 1
       fi
-      if [ ${2: -4} == ".jpg" ] || [ ${2: -4} == ".avi" ]; then
-        echo "Setting attachment file to $2"
-        ATTACHMENT="$2"
+      if [ ${ATTACHMENT: -4} == ".jpg" ] || [ ${ATTACHMENT: -4} == ".avi" ]; then
+        echo "Setting attachment file to $1 ..."
+        ATTACHMENT="$1"
+        ATTACHMENT_EXT=${ATTACHMENT: -4}
 
         # check attachment size and if needed try to compress it down
         # before trying to send (the MMS gateway will just scrub the
         # attachment otherwise)
-        filesize=$(du -k "$2" | cut -f 1)
+        filesize=$(du -k "$ATTACHMENT" | cut -f 1)
         if [ $filesize -ge $ATTACHMENT_MAX_SIZE ]; then
-          echo "Specified attachment is too large"
-          if [ ${2: -4} == ".avi" ]; then
+          echo >&2 "WARNING: Specified attachment is too large"
+          if [ "$ATTACHMENT_EXT" == ".avi" ]; then
+            # hash $FFMPEG_BIN 2>/dev/null || { echo >&2 "You need to install ffmpeg. Aborting."; exit 1; }
             if [ ! -f "$FFMPEG_BIN" ]; then
-              echo "Fatal error: could not locate ffmpeg binary at '$FFMPEG_BIN'"
+              echo >&2 "Fatal error: could not locate ffmpeg binary at '$FFMPEG_BIN'"
               exit 1
             fi
-            duration=$($FFMPEG_BIN -i "$2" 2>&1 | grep Duration | cut -d ' ' -f 4 | cut -d '.' -f 1)
+            duration=$($FFMPEG_BIN -i "$ATTACHMENT" 2>&1 | grep Duration | cut -d ' ' -f 4 | cut -d '.' -f 1)
             len_s=$(date +'%s' -d "$duration")
             bitrate=$(((($ATTACHMENT_MAX_SIZE / 1024) * 1024 * 1024) / len_s * 8))
             # bitrate=$(((($ATTACHMENT_MAX_SIZE * 1024) / len_s) * 8))
             echo "Calculated a required birate of $bitrate for video length of $len_s second(s)."
-            compressed_name="$2.small.avi"
+            compressed_name="$ATTACHMENT.small.avi"
             if [ ! -f "$compressed_name" ]; then
               echo "Compressing video stream to '$compressed_name' using '$FFMPEG_BIN' ..."
-              $FFMPEG_BIN -i "$2" \
+              $FFMPEG_BIN -i "$ATTACHMENT" \
                           -s 320x240 \
                           -b:v $bitrate \
                           -r "15" \
@@ -109,62 +204,77 @@ case "$1" in
             # use compressed attachment
             ATTACHMENT="$compressed_name"
           else
-            echo "Cannot compress filetype '${2: -4}' and it is too large to send, nothing left to do."
+            echo >&2 "Cannot compress filetype '$ATTACHMENT_EXT' and it is too large to send, nothing left to do."
             exit 1
           fi
         fi
       else
-        echo "Specified attachment not supported by MMS: $2"
+        echo >&2 "Specified attachment not supported by MMS: $1"
         exit 1
       fi
     fi
     shift
   ;;
   --mms-gateway)
-    echo "MMS_GATEWAY set to: $2"
-    MMS_GATEWAY="$2"
+    test_arg "$1" "$2"
+    shift
+    MMS_GATEWAY="$1"
+    echo "MMS_GATEWAY set to: $MMS_GATEWAY"
     shift
   ;;
   --txt-gateway)
-    echo "TXT_GATEWAY set to: $2"
-    MMS_GATEWAY="$2"
+    test_arg "$1" "$2"
+    shift
+    TXT_GATEWAY="$1"
+    echo "TXT_GATEWAY set to: $TXT_GATEWAY"
     shift
   ;;
   --smtp-server)
-    echo "SMTP_SERVER set to: $2"
-    SMTP_SERVER="$2"
+    test_arg "$1" "$2"
+    shift
+    SMTP_SERVER="$1"
+    echo "SMTP_SERVER set to: $SMTP_SERVER"
     shift
   ;;
   --force-mms)
     echo "FORCE_MMS set to true"
     FORCE_MMS="yes"
-    # shift
+    shift
+  ;;
+  -v|--verbose)
+    ((VERBOSITY++))
+    VERBOSE_SWITCH="-v"
+    shift
+  ;;
+  -h|--help)
+    usage
   ;;
   *)
     # unknown option
-    echo "Unknown option: $2"
+    echo "Unknown option: $1"
+    shift
   ;;
 esac
-shift
 done
 
 if [[ -z "$MOBILE" ]]; then
-  echo "A mobile number way not supplied."
+  echo >&2 "A mobile number way not supplied."
   exit 1
 fi
 
 # Send the alert using the mailx command.
+MAIL_ARGS="$VERBOSE_SWITCH"
 if [ -n "$FORCE_MMS" ] || [ -n "$ATTACHMENT" ]; then
   echo "Sending message using MMS gateway..."
   if [[ -z "$ATTACHMENT" ]]; then
-    echo -e "$MESSAGE"|mailx -v \
+    echo -e "$MESSAGE"|mailx $MAIL_ARGS \
     -r "$MAIL_FROM" \
     -s "$SUBJECT" \
     -S smtp="$SMTP_SERVER" \
     -S smtp-use-starttls \
     "$MOBILE@$MMS_GATEWAY"
   else
-    echo -e "$MESSAGE"|mailx -v -a "$ATTACHMENT" \
+    echo -e "$MESSAGE"|mailx $MAIL_ARGS -a "$ATTACHMENT" \
     -r "$MAIL_FROM" \
     -s "$SUBJECT" \
     -S smtp="$SMTP_SERVER" \
@@ -173,7 +283,7 @@ if [ -n "$FORCE_MMS" ] || [ -n "$ATTACHMENT" ]; then
   fi
 else
   echo "Sending message using TXT gateway..."
-  echo -e "$MESSAGE"|mailx -v \
+  echo -e "$MESSAGE"|mailx $MAIL_ARGS \
   -r "$MAIL_FROM" \
   -s "$SUBJECT" \
   -S smtp="$SMTP_SERVER" \
