e3f195ce75a963c624a36def5069dde00adc5eda
Robert W. Baumgartner <rwb@0x19e.net>
2015-11-27 15:52:06 -0600

    update pkt-capture script

--- net/pkt-capture.sh
+++ net/pkt-capture.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# invokes tcpdump on the specified interface
+
+hash tcpdump 2>/dev/null || { echo >&2 "You need to install tcpdump. Aborting."; exit 1; }
 
 if [[ -z "$1" ]] || [[ -z "$2" ]]; then
   echo "Usage: $0 <iface> <file>"
