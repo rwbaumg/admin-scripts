8772871f78bb37a0b861cdda95ef9dad08d839d7
Robert W. Baumgartner <rwb@0x19e.net>
2015-11-29 01:38:11 -0600

    sys-call-table: check for root

--- sys-call-table.sh
+++ sys-call-table.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+# display the syscall table
+
+# check if superuser
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" >&2
+   exit 1
+fi
+
 cat /boot/System.map-$(uname -r) | grep sys_call_table | cut -d " " -f 1
