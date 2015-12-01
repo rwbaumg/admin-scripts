56f23249bab0bae67c972896dcba0c1a60936467
Robert W. Baumgartner <rwb@0x19e.net>
2015-11-30 20:23:25 -0600

    ls-chmod: use full directory listing

--- ls-chmod.sh
+++ ls-chmod.sh
@@ -2,9 +2,9 @@
 # list file and folder permissions
 
 if [[ -z "$1" ]]; then
-  ls -l | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/) \
+  ls -lah | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/) \
               *2^(8-i));if(k)printf("%0o ",k);print}'
 else
-  ls -l "$1" | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/) \
+  ls -lah "$1" | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/) \
                    *2^(8-i));if(k)printf("%0o ",k);print}'
 fi
