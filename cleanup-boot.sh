5bd0d10ee7efc08e6c1dd155a0de996319ae95fb
Robert W. Baumgartner <rwb@0x19e.net>
2015-12-07 23:30:57 -0600

    cleanup-boot: add warning message, cleanup whitespace

--- cleanup-boot.sh
+++ cleanup-boot.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
 # Clean up the /boot partition
-dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
+# DANGER: This script is dangerous!
+# Only run it if you know what you
+# are doing.
+
+dpkg -l 'linux-*' \
+| sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' \
+| xargs sudo apt-get -y purge
