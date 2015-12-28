5b649edbee33d38cea5218ad8051419c609ef9e5
Robert W. Baumgartner <rwb@0x19e.net>
2015-12-28 14:06:20 -0600

    add sysload script

---
+++ sysload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# print an overview of current system load
+
+iostat -m
+
+free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
+df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'
+top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}'
+
+echo -e "\nProcess memory usage:"
+ps axo rss,comm,pid | awk '{ proc_list[$2]++; proc_list[$2 "," 1] += $1; } END { for (proc in proc_list) { printf("%d\t%s\n", proc_list[proc "," 1],proc); }}' | sort -n | tail -n 10
+# ps -eo user,pcpu,pmem | tail -n +2 | awk '{num[$1]++; cpu[$1] += $2; mem[$1] += $3} END{printf("NPROC\t\tUSER\tCPU\tMEM\n"); for (user in cpu) printf("%d\t%12s\t%.2f\t%.2f\n",num[user], user, cpu[user], mem[user]) }'
