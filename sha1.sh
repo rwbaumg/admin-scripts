e6dc210ace15ed0a7e4ccdd66cff1a7d1126a171
Robert W. Baumgartner <rwb@0x19e.net>
2016-01-12 21:44:00 -0600

    sha1: handle piped input

--- sha1.sh
+++ sha1.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # gets the sha1 hash of an input string
 
-if [[ -z "$1" ]]; then
-  echo "Usage: $0 <string>" >&2
-  exit 1
+if [ -z "$1" ]; then
+  cat | sha1sum | awk '{printf "\n%s\n\n", $1}'
+else
+  echo -n "$1" | sha1sum | awk '{printf "\n%s\n\n", $1}'
 fi
-
-echo -n "$1" | sha1sum | awk '{print $1}'
