a5ffa7545f44a6408fee6c8c548cbd2915157b74
slynx <slynx@0x19e.net>
2016-02-05 14:40:19 -0600

    add second command to list-iptables-attackers (not currently used)

--- net/list-iptables-attackers.sh
+++ net/list-iptables-attackers.sh
@@ -9,3 +9,5 @@ grep "iptables dropped:" $LOGPATTERN \
   | uniq -c \
   | sort -rn \
   | head -n 50
+
+# dmesg | grep -P '^\[\d+\.\d+\]\siptables dropped:\sIN\=([A-Za-z0-9\.]+)\sOUT\=([A-Za-z0-9\.]+)?\sPHYSIN\=([A-Za-z0-9\.]+)?\sMAC\=([0-9A-Fa-f]{2}[:-]){13}([0-9A-Fa-f]{2})\sSRC\=(?:[0-9]{1,3}\.){3}[0-9]{1,3}\sDST\=(?:[0-9]{1,3}\.){3}[0-9]{1,3}\sLEN\=(\d+)\sTOS\=(0[xX][0-9a-fA-F]+)\sPREC\=(0[xX][0-9a-fA-F]+)\sTTL\=(\d+)\sID\=(\d+)\sPROTO\=(TCP|UDP)\sSPT\=(\d+)\sDPT\=(\d+)\sWINDOW\=\d\sRES\=(0[xX][0-9a-fA-F]+)\s(RST\s)?(URGP\=\d)?'
