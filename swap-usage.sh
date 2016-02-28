0ee4401520ec58852d7cb52a644ce28b4a7f8dc4
Robert W. Baumgartner <rwb@0x19e.net>
2016-02-28 17:14:28 -0600

    update swap-usage; refactor to output acsii table

--- swap-usage.sh
+++ swap-usage.sh
@@ -2,11 +2,17 @@
 # shows system swap usage
 # rwb[at]0x19e[dot]net
 
+# check if awk command exists
+hash awk 2>/dev/null || { echo >&2 "You need to install awk. Aborting."; exit 1; }
+
+# print table header
+printf '=%.0s' {1..50}
+printf '\n'
+printf "%-9s %-21s %s\n" "PID" "Name" "Swap used"
+printf '=%.0s' {1..50}
+printf '\n'
+
 SWAP_TOTAL=0
-
-echo "Swap usage"
-echo "----------"
-
 for file in /proc/*/status ; do
   NAME=$(awk '/^Name/{$1=""; print $0} END { print ""}' $file)
   PID=$(awk '/^Pid/{printf $2} END { print ""}' $file)
@@ -15,13 +21,16 @@ for file in /proc/*/status ; do
   if [ -n "$VMSWAP" ]; then
     if [ $VMSWAP -gt 0 ]; then
       let SWAP_TOTAL+=$VMSWAP
-      echo "$NAME: ${VMSWAP}kb"
-      # echo "$NAME ($PID): ${VMSWAP}kb"
+      printf "%-8s %-22s %s\n" "${PID}" "${NAME}" "${VMSWAP}kb"
     fi
   fi
 done
 
-echo "----------"
-echo "Total: ${SWAP_TOTAL}kb"
+# print footer including total usage
+printf '=%.0s' {1..50}
+printf '\n'
+printf "%-9s %-21s %s\n" "" "Total:" "${SWAP_TOTAL}kb"
+printf '=%.0s' {1..50}
+printf '\n'
 
 exit 0
