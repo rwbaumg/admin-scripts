c31df0521bb74a037bb3fc63bfc744ecdf2ccbaf
Robert W. Baumgartner <rwb@0x19e.net>
2016-02-28 17:17:28 -0600

    change-ext: fix argument check

--- change-ext.sh
+++ change-ext.sh
@@ -3,6 +3,7 @@
 
 if [ "$#" -ne 3 ]; then
   echo >&2 "Usage: $0 <path> <ext> <new_ext>"
+  exit 1
 fi
 
 echo "Updating extensions in $1 ..."
@@ -11,3 +12,5 @@ echo "Converting $2 -> $3"
 for f in *.$2; do
   mv -v "$f" "$(basename "$f" .$2).$3"
 done
+
+exit 0
