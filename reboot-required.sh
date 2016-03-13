34733372e46eaf5695cd6e6cb6c88a9d4a616096
slynx <slynx@0x19e.net>
2016-03-13 15:54:09 -0500

    reboot-required: add dependency check for update-notifier-common

--- reboot-required.sh
+++ reboot-required.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # checks if the system is pending a reboot
 
-if [ -f /var/run/reboot-required ]; then
-    cat /var/run/reboot-required
-    exit 0
+if [ ! -e /etc/cron.daily/update-notifier-common ]; then
+  echo >&2 "This script depends on update-notifier-common."
+  exit 1
 fi
+
+if [ -f /var/run/reboot-required ]; then
+  cat /var/run/reboot-required
+  exit 0
+fi
+
 exit 1
