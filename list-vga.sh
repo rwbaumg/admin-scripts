8538143dc700f7fca9f77dce4cb5945666bae112
rwb <rwb@0x19e.net>
2017-03-25 21:12:15 -0500

    add list-vga.sh script (lists vga pci devices)

---
+++ list-vga.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# List PCI devices
+
+# list pci id, class, vendor and device ids
+# for device in /sys/bus/pci/devices/*; do echo "$(basename ${device} | cut -c '6-') $(cut -c '3-6' ${device}/class): $(cut -c '3-' ${device}/vendor):$(cut -c '3-' ${device}/device)"; done
+
+# list vga devices
+for I in `lspci |awk '/VGA/{print $1}'`;do lspci -v -s $I;done
+# lspci -v | perl -ne '/VGA/../^$/ and /VGA|Kern/ and print'
