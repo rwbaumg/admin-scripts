ac9d66d27672c57570b6c94f5add5469f2f9a40b
Robert W. Baumgartner <rwb@0x19e.net>
2017-04-01 17:15:22 -0500

    Update net/gen-mac.sh to have a custom first-octet value.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- net/gen-mac.sh
+++ net/gen-mac.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Generate a new MAC address given a hostname
 
-MACADDR=$(echo "$1"|sha1sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/')
+# note: first octet must be event according to spec
+FIRST_OCTET="88"
+MACADDR=$(echo "$1"|sha1sum|sed "s/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/${FIRST_OCTET}:\1:\2:\3:\4:\5/")
 echo $MACADDR
