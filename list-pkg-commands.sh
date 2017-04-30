7f5f948502f725f31fc3292cd28bbb5e652ea6eb
Robert W. Baumgartner <rwb@0x19e.net>
2017-04-29 19:29:55 -0500

    add list-pkg-commands.sh script (not complete)

---
+++ list-pkg-commands.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# TODO: Check if package is installed
+
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+
+# print table header
+printf "$COL_RESET"
+printf '=%.0s' {1..50}
+printf '\n'
+printf "$COL_RESET%-20s %s \n$COL_RESET" "Command" "Description"
+printf '=%.0s' {1..50}
+printf '\n'
+printf "$COL_RESET"
+
+for d in `dpkg -L $1 | grep bin/ | sort`; do \
+  echo $(man $d | head -4 | tail -n1 ) \
+    | awk -F'—|-|,' -v N=2 'BEGIN {OFS="\t"}; { \
+      printf("%-20s", $1); \
+      OFS=" "; sep=""; for (i=N; i<=NF; i++) { \
+        printf("%s%s",sep,$i); sep=OFS }; \
+        printf("\n"); } \
+    ' ; \
+  done
