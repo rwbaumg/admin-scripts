023bbe5103ed1cd71f32653d51d93fe2aa93b0fa
Robert W. Baumgartner <rwb@0x19e.net>
2017-06-04 16:51:12 -0500

    list-pkg-commands: fix awk statement

--- list-pkg-commands.sh
+++ list-pkg-commands.sh
@@ -22,8 +22,8 @@ printf '\n'
 printf "$COL_RESET"
 
 for d in `dpkg -L $1 | grep bin/ | sort`; do \
-  echo $(man $d | grep NAME -A1 | head -2 | tail -n1 ) \
-    | awk -F'—|-|,' -v N=2 'BEGIN {OFS="\t"}; { \
+  echo $(man -P cat $d 2>/dev/null | grep NAME -A1 | head -2 | tail -n1 ) \
+    | awk -F' - ' -v N=2 'BEGIN {OFS=" "}; { \
       printf("%-20s", $1); \
       OFS=" "; sep=""; for (i=N; i<=NF; i++) { \
         printf("%s%s",sep,$i); sep=OFS }; \
