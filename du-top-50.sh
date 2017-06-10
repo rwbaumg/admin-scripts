b8078bb7728b0a200233b47948224e9eaae56159
Robert W. Baumgartner <rwb@0x19e.net>
2017-06-09 20:08:37 -0500

    refactor du-top scripts to use a spinner for long waits

--- du-top-50.sh
+++ du-top-50.sh
@@ -12,6 +12,50 @@ else
   echo "$TOP_50_MSG"
 fi
 
-du -hsx "${INPUT_PATH}"/* \
-  | sort -rh \
-  | head -50
+trap cleanup EXIT INT TERM
+cleanup() {
+  kill $SCRIPT_PID
+  exit $?
+}
+
+run_spinner()
+{
+  local msg="$1"
+  local delay=0.5
+  local i=1;
+  local SP='\|/-';
+  while [ "$SPIN" == "true" ]
+  do
+    printf "\b\r[${SP:i++%${#SP}:1}] $msg"
+    sleep $delay;
+  done
+}
+
+start_spinner()
+{
+  local arg="$1"
+
+  SPIN="true"
+  run_spinner "$1" &
+  SCRIPT_PID=$!
+}
+
+stop_spinner()
+{
+  SPIN="false"
+
+  # clear current line of any text
+  echo -ne "\r%\033[0K\r"
+}
+
+start_spinner "Calculating directory sizes ..."
+
+OUTPUT=$(du -hsx "${INPUT_PATH}"/* \
+           | sort -rh \
+           | head -50)
+
+stop_spinner
+
+echo "$OUTPUT"
+
+exit 0
