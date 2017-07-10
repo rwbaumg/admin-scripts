1232aa2962eed7b4342d0b6967d01fd2f419224c
slynx <slynx@0x19e.net>
2017-07-09 21:10:44 -0500

    update net/pkt-capture.sh argument handling

--- net/pkt-capture.sh
+++ net/pkt-capture.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 # invokes tcpdump on the specified interface
+# rwb[at]0x19e[dot]net
 
 hash tcpdump 2>/dev/null || { echo >&2 "You need to install tcpdump. Aborting."; exit 1; }
 
-if [[ -z "$1" ]] || [[ -z "$2" ]]; then
-  echo "Usage: $0 <iface> <file>"
+OPTS="-nnXSs 1514"
+
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 <iface> [file]"
   exit 1
 fi
 
-tcpdump -nnXSs 1514 -w $2 -i $1
+if [[ -z "$2" ]]; then
+  OUTPUT="${1}-$(date '+%Y%m%d%H%M%S').pcap"
+else
+  OUTPUT="$2"
+fi
+
+echo "Capturing $1 to '$OUTPUT' ..."
+tcpdump $OPTS -w "$OUTPUT" -i $1
