e84047bcb8fb4b1bdd007daad45b4efa8af0948a
Robert W. Baumgartner <rwb@0x19e.net>
2017-07-21 19:53:22 -0500

    security: update gen-passwd.sh script

--- security/gen-passwd.sh
+++ security/gen-passwd.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # generates a random password
+# uses /dev/random to ensure good random numbers
 
+RNG_DEV="/dev/random"
+
+# Strong passwords should be at least this long
 DEFAULT_LENGTH="20"
+
+# Standard charset is alphanumeric with upper and
+# lower case w/ some special characters
 CHARSET="A-Za-z0-9_"
-#CHARSET="A-Za-z0-9_\$#!@&"
+
+# Use the below charset for higher entropy
+# CHARSET="A-Za-z0-9_\$#!@&%^*~=+,.<>[]/\\\"\'\`"
 
 LENGTH="$1"
 re='^[0-9]+$'
@@ -11,4 +20,4 @@ if ! [[ $LENGTH =~ $re ]] ; then
   LENGTH=$DEFAULT_LENGTH
 fi
 
-tr -dc "$CHARSET" < /dev/urandom | head -c $LENGTH | xargs
+tr -dc "$CHARSET" < $RNG_DEV | head -c $LENGTH | xargs -0
