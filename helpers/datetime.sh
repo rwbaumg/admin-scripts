5568ebe7db145f5e96cd0a2377a585902b3c057b
Robert W. Baumgartner <rwb@0x19e.net>
2017-11-05 00:29:20 -0500

    add datetime helper

---
+++ helpers/datetime.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Date/time helpers
+
+function countdown(){
+   start_date=$((`date +%s` + $1));
+   while [ "$start_date" -ge `date +%s` ]; do
+     echo -ne "$(date -u --date @$(($start_date - `date +%s`)) +%H:%M:%S)\r";
+     sleep 0.1
+   done
+}
+
+function stopwatch(){
+  start_date=`date +%s`;
+   while true; do
+    echo -ne "$(date -u --date @$((`date +%s` - $start_date)) +%H:%M:%S)\r";
+    sleep 0.1
+   done
+}
