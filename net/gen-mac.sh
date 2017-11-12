7bf3dd77702b360767ba5bcad29bae087e485270
Robert W. Baumgartner <rwb@0x19e.net>
2017-11-12 15:05:16 -0600

    refactor net/gen-mac.sh script to correctly handle mfg prefixes

--- net/gen-mac.sh
+++ net/gen-mac.sh
@@ -2,6 +2,18 @@
 # Generate a new MAC address given a hostname
 
 # note: first octet must be event according to spec
-FIRST_OCTET="88"
-MACADDR=$(echo "$1"|sha1sum|sed "s/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/${FIRST_OCTET}:\1:\2:\3:\4:\5/")
+# the Xensource id is 00:16:3e
+# the x19e id is 30:16:c6
+# FIRST_OCTET="88"
+# MFG_ID="00:16:3e"
+MFG_ID="30:16:c6"
+SEED_VALUE="$1"
+if [[ -z "$SEED_VALUE" ]]; then
+  SEED_VALUE=$(uuidgen)
+fi
+if [[ ! -z "$FIRST_OCTET" ]]; then
+  MACADDR=$(echo "$SEED_VALUE"|sha1sum|sed "s/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/${FIRST_OCTET}:\1:\2:\3:\4:\5/")
+else
+  MACADDR=$(echo "$SEED_VALUE"|sha1sum|sed "s/^\(..\)\(..\)\(..\).*$/${MFG_ID}:\1:\2:\3/")
+fi
 echo $MACADDR
