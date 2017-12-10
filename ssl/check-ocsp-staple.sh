ad2a48ce817cb8bd07f20eb76a4e5813fae94cbd
slynx <slynx@0x19e.net>
2017-12-09 18:38:16 -0600

    ssl: update check-ocsp-staple.sh script

--- ssl/check-ocsp-staple.sh
+++ ssl/check-ocsp-staple.sh
@@ -8,9 +8,45 @@ if [[ -z "$1" ]]; then
   exit 1
 fi
 
-SERVER="$1"; \
-echo QUIT | openssl s_client -connect $SERVER:443 \
-                             -servername $SERVER \
-                             -status 2> /dev/null \
-          | grep -A 17 'OCSP response:' \
-          | grep --color=never -B17 -A1 'This Update'
+SERVER="$1"
+SSL_PORT=443
+
+ping_test()
+{
+  if [[ -z "$1" ]]; then
+    echo 2
+    return
+  fi
+
+  # todo: inveistigate fping
+  # fping -c1 -t300 $IP
+  local hostname="$1"
+
+  if ! ping -c 1 $hostname > /dev/null 2>&1; then
+    echo >&2 "$SERVER is not rresponding to ping requests and may be down."
+    exit 3
+  fi
+}
+
+ping_test $SERVER
+
+SSL_RESULT=$(echo QUIT | openssl s_client -connect $SERVER:$SSL_PORT \
+                                          -servername $SERVER \
+                                          -status > /dev/null 2>&1)
+if [ $? -eq 0 ]; then
+    echo "Got response from $SERVER:$SSL_PORT (server name $SERVER)"
+else
+    echo >&2 "Failed to get response from server $SERVER:$SSL_PORT"
+    exit $?
+fi
+OCSP_RESPONSE=$(echo $SSL_RESULT | grep -A 17 'OCSP response:' \
+                                 | grep --color=never -B17 -A1 'This Update')
+
+if [[ -z "$OCSP_RESPONSE" ]]; then
+  echo >&2 "The server does not appear to have OCSP stapling."
+  exit 2
+fi
+
+echo $OCSP_RESPONSE
+
+exit 0
