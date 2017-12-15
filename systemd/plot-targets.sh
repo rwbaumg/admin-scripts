ad702abaf57e996c247785b71acfb0cd1d4daaa8
Robert W. Baumgartner <rwb@0x19e.net>
2017-12-14 20:10:19 -0600

    add systemd plot scripts

---
+++ systemd/plot-targets.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# creates a plot of systemd targets
+
+hash systemd-analyze 2>/dev/null || { echo >&2 "This system does not appear to use systemd (the systemd package was not detected). Aborting."; exit 1; }
+
+# determine the directory this script is in and store it in SCRIPT_DIR
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where
+  # the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+OUT_FILE="$SCRIPT_DIR/targets.svg"
+
+echo "Generating systemd targets plot file $OUT_FILE ..."
+
+systemd-analyze dot --to-pattern='*.target' --from-pattern='*.target' | dot -Tsvg > $OUT_FILE
+
+echo " done."
+
+exit 0
