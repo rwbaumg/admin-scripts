d92b347e96a402c00cbe99a493e031312c298b9b
Robert W. Baumgartner <rwb@0x19e.net>
2017-12-19 22:10:18 -0600

    add package checks to systemd plot scripts using graphiz

--- systemd/plot-targets.sh
+++ systemd/plot-targets.sh
@@ -2,6 +2,7 @@
 # creates a plot of systemd targets
 
 hash systemd-analyze 2>/dev/null || { echo >&2 "This system does not appear to use systemd (the systemd package was not detected). Aborting."; exit 1; }
+hash dot 2>/dev/null || { echo >&2 "You need to install graphviz. Aborting."; exit 1; }
 
 # determine the directory this script is in and store it in SCRIPT_DIR
 SOURCE="${BASH_SOURCE[0]}"
