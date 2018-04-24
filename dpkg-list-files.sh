b427e2baaad1d6014c458ba202a66a8b1f90563d
Robert W. Baumgartner <rwb@0x19e.net>
2018-04-23 23:23:54 -0500

    add argument check to dpkg-list-files.sh script

--- dpkg-list-files.sh
+++ dpkg-list-files.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 DEBFILE="$1"
+
+if [[ ! -f "$DEBFILE" ]]; then
+  echo >&2 "ERROR: Must supply path to package."
+  exit 1
+fi
+
 dpkg -c $DEBFILE | awk '{ print $6 }' | sort
+exit 0
