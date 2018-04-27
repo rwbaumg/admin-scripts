c6df22e317d860bfcb8c2176a5e6e590059d3750
slynx <slynx@0x19e.net>
2018-04-26 19:55:03 -0500

    add warning to clean-kernel-modules.sh

--- clean-kernel-modules.sh
+++ clean-kernel-modules.sh
@@ -8,6 +8,7 @@ if [[ ! -z "$1" ]]; then
 fi
 
 PATTERN=$TMP bash -c 'echo "# clean kernel modules matching pattern \"$PATTERN\"";
+                      echo "# WARNING: DOUBLE-CHECK COMMANDS BEFORE RUNNING!";
                       for f in `ls -d /lib/modules/$PATTERN | grep -v $(uname -r)`; do
                       echo sudo rm -v -r $f; done'
 
