a7f35d575023d0bd82147a3d1092a407946eb539
Robert W. Baumgartner <rwb@0x19e.net>
2018-06-05 17:41:42 -0500

    cleanup net/get-public-ip.sh

--- net/get-public-ip.sh
+++ net/get-public-ip.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # get public ip address using ipinfo.io api
 
+SERVICE_URL="http://ipinfo.io/ip"
+# SERVICE_URL="http://ifconfig.me/ip"
+
 if hash curl 2>/dev/null; then
-  curl ipinfo.io/ip
+  curl $SERVICE_URL
   exit 0
 else
   if hash wget 2>/dev/null; then
-    wget -qO- ipinfo.io/ip
+    wget -qO- $SERVICE_URL
     exit 0
   fi
 fi
@@ -14,6 +17,6 @@ fi
 # another way using just dns:
 # dig +short myip.opendns.com @resolver1.opendns.com
 
-echo "you must install either curl or wget"
+echo >&2 "you must install either curl or wget"
 
 exit 1
