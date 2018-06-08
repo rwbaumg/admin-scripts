7baa49e496b6ff8f59d8d4c3924228456ebc0486
Robert W. Baumgartner <rwb@0x19e.net>
2018-06-08 18:19:43 -0500

    complete updating x11vnc wrapper to support autostart

--- x11vnc.sh
+++ x11vnc.sh
@@ -16,6 +16,7 @@ hash x11vnc 2>/dev/null || { echo >&2 "You need to install x11vnc. Aborting."; e
 # . $(dirname $0)/helpers/log4bash.sh
 # /run/user/122/gdm/Xauthority
 
+DEBUG=0
 CURRENT_UID=$(id -u $USER)
 if [[ $CURRENT_UID == 0 ]]; then
   logger -p syslog.warn "WARNING: x11vnc found UID==0; forcing gdm UID"
@@ -23,24 +24,33 @@ if [[ $CURRENT_UID == 0 ]]; then
   CURRENT_UID=$(id -u gdm)
 fi
 
-AUTH_COOKIE="/run/user/$CURRENT_UID/gdm/Xauthority"
-LOG_FILE="/tmp/x11vnc-$USER.log"
-# LOG_FILE="/home/$USER/x11vnc.log"
+CURRENT_USER="$USER"
+if [[ -z "$CURRENT_USER" ]]; then
+  CURRENT_USER=$(getent passwd $CURRENT_UID | awk -F: '{ print $1 }')
+fi
+if [[ -z "$CURRENT_USER" ]]; then
+  logger -p syslog.error "ERROR: x11vnc could not identify the user to run under."
+  echo >&2 "ERROR: x11vnc could not identify the user to run under."
+fi
 
-logger -p syslog.info "Starting x11vnc for user $USER (uid $CURRENT_UID) ..."
+AUTH_COOKIE="/run/user/$CURRENT_UID/gdm/Xauthority"
+LOG_FILE="/tmp/x11vnc-$CURRENT_USER.log"
+# LOG_FILE="/home/$CURRENT_USER/x11vnc.log"
+
+logger -p syslog.info "Starting x11vnc for user $CURRENT_USER (uid $CURRENT_UID) ..."
 logger -p syslog.info "Logging to $LOG_FILE"
 
 # check if x11vnc is already running
 VNC_PID=$(ps -ef | grep "[x]11vnc" | grep $CURRENT_UID | awk '{ print $2}')
 if [[ ! -z "$VNC_PID" ]]; then
-  logger -p syslog.warn "ERROR: x11vnc appears to be running already for UID $CURRENT_UID."
-  echo >&2 "ERROR: x11vnc appears to be running already for UID $CURRENT_UID."
+  logger -p syslog.error "ERROR: x11vnc appears to be running already for UID $CURRENT_UID (pid $VNC_PID)."
+  echo >&2 "ERROR: x11vnc appears to be running already for UID $CURRENT_UID (pid $VNC_PID)."
   exit 1
 fi
 
 DISP_ARG="-display $DISPLAY"
 if [[ -z "$DISPLAY" ]]; then
-  logget -p syslog.info "WARNING: x11vnc could not detect display; using -find instead"
+  logger -p syslog.warn "WARNING: x11vnc could not detect display; using -find instead"
   echo >&2 "WARNING: x11vnc could not detect display; using -find instead"
   DISP_ARG="-find"
 fi
@@ -54,11 +64,13 @@ fi
 PW_OPT=""
 if [[ -e /etc/x11vnc.pass ]]; then
   PW_OPT="-rfbauth /etc/x11vnc.pass"
+  logger -p syslog.info "x11vnc using authentication file /etc/x11vnc.pass"
+  echo >&2 "x11vnc using authentication file /etc/x11vnc.pass"
 fi
 
 # note: use -logappend for persistent logging
-x11vnc -auth $AUTH_COOKIE \
-       $DISP_ARG \
+x11vnc -bg $DISP_ARG \
+       -auth $AUTH_COOKIE \
        -logfile $LOG_FILE \
        -usepw $PW_OPT \
        -forever \
@@ -69,20 +81,37 @@ if [[ -z "$VNC_PID" ]]; then
   logger -p syslog.error "ERROR: x11vnc failed to start."
   echo >&2 "ERROR: x11vnc failed to start."
   exit 3
+else
+  logger -p syslog.error "Detected x11vnc running on pid $VNC_PID, waiting for socket..."
+  echo >&2 "Detected x11vnc running on pid $VNC_PID, waiting for socket..."
 fi
 
 # give the socket a moment to bind
-sleep 1
+sleep 2
 
 # get the port number
-PORT=$(lsof -nPi | grep "IPv4" | grep "$VNC_PID" | awk '{print $9}' | awk -F":" '{print $2}')
+PORT=$(netstat -4 -an --tcp --program 2> /dev/null | grep "$VNC_PID" | awk '{print $4}' | awk -F":" '{print $2}')
+# PORT=$(lsof -nPi | grep "IPv4" | grep "$VNC_PID" | awk '{print $9}' | awk -F":" '{print $2}')
 
-# check to make sure value is a valid port
+# DEBUG: log all ports visible at this point in the script
+if [[ "$DEBUG" == 1 ]]; then
+  TEST=$(netstat -4 -an --tcp --program)
+  logger -p syslog.error "x11vnc is able to see the following ports: $TEST"
+fi
+
+# check if we were able to find the listen port, and if so, validate it
 re='^[0-9]+$'
 if ! [[ $PORT =~ $re ]] ; then
-  logger -p syslog.error "ERROR: '$PORT' is not a valid port number"
-  echo >&2 "ERROR: '$PORT' is not a valid port number"
-  exit 4
+  if ! [[ -z $PORT ]]; then
+    logger -p syslog.error "ERROR: '$PORT' is not a valid x11vnc port number."
+    echo >&2 "ERROR: '$PORT' is not a valid x11vnc port number."
+    exit 4
+  else
+    # port is null but x11vnc is running
+    PORT="?"
+    logger -p syslog.warn "WARNING: Unable to determine which port x11vnc is listening on."
+    echo >&2 "WARNING: Unable to determine which port x11vnc is listening on."
+  fi
 fi
 
 logger -p syslog.info "x11vnc started on display $DISPLAY; listening on 127.0.0.1:$PORT"
