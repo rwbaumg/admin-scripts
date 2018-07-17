a13330704dc23cca55b569e7be462c965d3e9290
Robert W. Baumgartner <rwb@0x19e.net>
2018-07-17 14:28:00 -0500

    add pi-bench.sh

---
+++ pi-bench.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# a simple cpu benchmark using PI calculation
+
+CPU="${1:-1}";
+SCALE="${2:-5000}";
+
+echo "Cores: $CPU"; echo "Digit: $SCALE" ;
+for LOOP in `seq 1 $CPU`; do
+  time echo "scale=${SCALE}; 4*a(1)" | bc -l -q | grep -v ^"[0-9]"
+done
