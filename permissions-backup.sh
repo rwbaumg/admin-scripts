6e27b42efcba875786f2f32529b31e0c5be3e552
Robert W. Baumgartner <rwb@0x19e.net>
2018-08-03 19:24:46 -0500

    add permissions-backup.sh script

---
+++ permissions-backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# creates a backup of all file and folder ownership and permissions
+# output is stored in the format 'user:group chmod path'
+
+# check if superuser
+if [[ $EUID -ne 0 ]]; then
+   echo >&2 "This script must be run as root."
+   exit 1
+fi
+
+TODAY=$(date +"%Y-%m-%d")
+
+find / -name '*' -printf '%u:%g %m %p\n' > permissions-backup-${TODAY}.txt
+
+exit 0
