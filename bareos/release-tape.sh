0160bfb09a3afe096cafdfb769cd630b787fb72a
Robert W. Baumgartner <rwb@0x19e.net>
2018-09-07 22:03:04 -0500

    bareos: update release-tape.sh bconsole output handling

--- bareos/release-tape.sh
+++ bareos/release-tape.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-# releases the tape
-bconsole <<END_OF_DATA
-release storage=Tape
+# release tape storage
+#
+STORAGE="Tape"
+
+# Use bconsole to release storage
+bconsole <<END_OF_DATA 2>&1 >/dev/null
+@output /dev/null
+@output
+release storage=${STORAGE}
+quit
 END_OF_DATA
+
+# Check to make sure the command succeeded
+if ! [ $? -eq 0 ]; then
+  echo >&2 "ERROR: Failed to release ${STORAGE} storage."
+  exit 1
+fi
+
+exit 0
