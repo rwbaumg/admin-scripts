077f88b89de961bedf8010eaafcf75648f8c7bb4
Robert W. Baumgartner <rwb@0x19e.net>
2018-09-19 03:34:16 -0500

    add dpkg-purge-uninstalled.sh script

---
+++ dpkg-purge-uninstalled.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Purges configurations for uninstalled packages
+# WARNING: This script deletes data! Make sure you have backups.
+
+sudo dpkg --purge `COLUMNS=300 dpkg -l "*" | egrep "^rc" | cut -d\  -f3`
+
+exit 0
