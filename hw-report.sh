0d49904678eada21be463658c32f2d839543f17f
Robert W. Baumgartner <rwb@0x19e.net>
2018-11-05 16:30:55 -0600

    fix hw-report.sh output file permissions
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- hw-report.sh
+++ hw-report.sh
@@ -5,18 +5,20 @@
 #  `REPORT_PATH=$(sudo ./hw-report.sh 2>/dev/null)`
 
 # check if superuser
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" >&2
-   exit 1
-fi
+#if [[ $EUID -ne 0 ]]; then
+#   echo "This script must be run as root" >&2
+#   exit 1
+#fi
 
 OUTPUT="hardware.html"
 
-lshw -html > "${OUTPUT}"
+RAW=$(sudo lshw -html)
 if ! [ $? -eq 0 ]; then
   echo >&2 "ERROR: Failed to generate hardware report."
   exit 1
 fi
+
+echo "${RAW}" > "${OUTPUT}"
 
 echo >&2 "Saved hardware report to $(basename ${OUTPUT})"
 echo "$(readlink -f ${OUTPUT})"
