aadb7ded62fc60ac79dbeeda1c2c75d86cc62615
Robert W. Baumgartner <rwb@0x19e.net>
2018-11-15 17:32:29 -0600

    fix filename-cleanup.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- filename-cleanup.sh
+++ filename-cleanup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-MV_CMD="mv -v"
-# MV_CMD="echo mv -v"
+DRY_RUN="true"
 
 # print out commands to clean filenames in the current directory
-for i in *' '*; do \
-   ${MV_CMD} "$i" `echo $i | sed -e 's/ /_/g' -e 's/(//g' -e 's/)//g' `; \
+for i in *; do
+  if `echo $i | grep -Po '\s'`; then
+    if [ "$DRY_RUN" == "false" ]; then
+      mv -v "$i" `echo $i | sed -e 's/ /_/g' -e 's/(//g' -e 's/)//g' `;
+    else
+      echo mv -v \"$i\" `echo $i | sed -e 's/ /_/g' -e 's/(//g' -e 's/)//g' `;
+    fi
+  fi
 done
 
 exit 0
