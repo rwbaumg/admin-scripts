c825fa2cd3ab56e8dd64209c3aa21928a5bce40b
Robert W. Baumgartner <rwb@0x19e.net>
2019-02-21 23:13:18 -0600

    security: add gpg-test-key.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

---
+++ security/gpg-test-key.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# tests gpg encryption/decryption for the specified key
+
+hash gpg 2>/dev/null || { echo >&2 "You need to install gnupg. Aborting."; exit 1; }
+
+if [[ -z "$1" ]]; then
+  echo >&2 "Usage: $0 <key_id>"
+  exit 1
+fi
+
+KEY_ID="$1"
+EXTRA_OPTS="--verbose"
+#EXTRA_OPTS="--verbose --debug-all --debug-level guru"
+
+echo "Testing GnuPG key with fingerprint ID $1 ..."
+echo "$(uname -a)" | gpg ${EXTRA_OPTS} --encrypt --armor --recipient ${KEY_ID} | gpg ${EXTRA_OPTS} --decrypt --armor
+
+exit 0
