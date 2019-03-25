de50ea307d68ba7dfaf38e83a99e0680ac39cd7b
Robert W. Baumgartner <rwb@0x19e.net>
2019-03-25 01:52:10 -0500

    bareos: fix bugs in install-client.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- bareos/install-client.sh
+++ bareos/install-client.sh
@@ -5,34 +5,51 @@ hash wget 2>/dev/null || { echo >&2 "You need to install wget. Aborting."; exit 
 hash apt-get 2>/dev/null || { echo >&2 "You need to install apt. Aborting."; exit 1; }
 
 UBUNTU_RELEASE=$(lsb_release -a 2>/dev/null | grep Release | awk '{print $2}')
+if [ -z "${UBUNTU_RELEASE}" ]; then
+  echo >&2 "ERROR: Unable to determine Ubuntu release."
+  exit 1
+fi
 
 echo "Installing Bareos FileDaemon for Ubuntu v${UBUNTU_RELEASE} backup client..."
 
+HTPROTO="http"
+
 PKG_LST="/etc/apt/sources.list.d/bareos.list"
-PKG_SRC="deb http://download.bareos.org/bareos/release/latest/xUbuntu_${UBUNTU_RELEASE}/ ./"
-PKG_KEY="http://download.bareos.org/bareos/release/latest/xUbuntu_${UBUNTU_RELEASE}/Release.key"
+PKG_SRC="download.bareos.org/bareos/release/latest/xUbuntu_${UBUNTU_RELEASE}/ ./"
+PKG_KEY="download.bareos.org/bareos/release/latest/xUbuntu_${UBUNTU_RELEASE}/Release.key"
+
+DEB_TXT="deb ${HTPROTO}://${PKG_SRC}"
+KEY_URL="${HTPROTO}://${PKG_KEY}"
 
 # add the package source
 echo "Configuring package source in list file ${PKG_LIST} ..."
-grep -qF "${PKG_SRC}" "${PKG_LST}"  || echo "${PKG_SRC}" | sudo tee -a "${PKG_LST}"
-if ! [ $? -eq 0 ]; then
-  exit 1
+if [ ! -e "${PKG_LST}" ] || ! grep -qF "${PKG_SRC}" "${PKG_LST}"; then
+  echo "${DEB_TXT}" | sudo tee -a "${PKG_LST}"
+  if ! [ $? -eq 0 ]; then
+    exit 1
+  fi
 fi
 
 # add the release key
 echo "Retrieve release signing key ..."
-wget -qO - "${PKG_KEY}"  | sudo apt-key add -
+wget -qO - "${KEY_URL}"  | sudo apt-key add -
 if ! [ $? -eq 0 ]; then
   exit 1
 fi
 
+# git handling for etckeeper (check if /etc/.git exists)
 if [ -d /etc/.git  ] && hash git 2>/dev/null; then
-  # commit /etc changes
-  echo "Auto-commit changes to /etc (directory under version control) ..."
-  pushd /etc
-  sudo git add --all /etc/apt
-  sudo git commit -v -m "apt: add bareos package source"
-  popd
+  if `git -C "/etc" rev-parse > /dev/null 2>&1`; then
+    # check /etc/apt for modifications
+    # if there are changes, commit them
+    if [[ "$(git --git-dir=/etc/.git --work-tree=/etc status --porcelain -- /etc/apt|egrep '^(M| M)')" != "" ]]; then
+      echo "Auto-commit changes to /etc/apt (directory under version control) ..."
+      pushd /etc > /dev/null 2>&1
+      sudo git add --all /etc/apt
+      sudo git commit -v -m "apt: add bareos package source"
+      popd > /dev/null 2>&1
+    fi
+  fi
 fi
 
 # update the package cache
@@ -46,6 +63,7 @@ fi
 echo "Running installation ..."
 sudo apt-get install bareos-filedaemon -y
 if ! [ $? -eq 0 ]; then
+  echo >&2 "ERROR: Failed to install Bareos client."
   exit 1
 fi
 
