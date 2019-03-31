e8252827f5433423461c0c723318390950dfbfd7
Robert W. Baumgartner <rwb@0x19e.net>
2019-03-31 18:06:50 -0500

    Add ps-ctxt.sh script for displaying process context switching stats.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

---
+++ ps-ctxt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Process context switching
+
+hash pidstat 2>/dev/null || { echo >&2 "You need to install sysstat. Aborting."; exit 1; }
+hash grep 2>/dev/null || { echo >&2 "You need to install grep. Aborting."; exit 1; }
+hash awk 2>/dev/null || { echo >&2 "You need to install gawk. Aborting."; exit 1; }
+
+pidstat -w 2 1 | grep Average | grep -v pidstat \
+  | sort -r -n -k4 \
+  | head -n 20 \
+  | awk '{ if ($3 != "PID") printf("ctxt sample: PID:%s\t\t%s\tcswch/s\t\t%s\tnvcswch/s\t%s\n", $3, $4, $5, $6) }'
+
+exit $?
