58a9e2c467057c6d1e7bc7a7af60f161fbcb0ca6
Robert W. Baumgartner <rwb@0x19e.net>
2019-04-16 22:52:53 -0500

    git: update git-stats.sh script to include contributors.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- git/git-stats.sh
+++ git/git-stats.sh
@@ -27,4 +27,8 @@ git log --shortstat \
   | grep -E "fil(e|es) changed" \
   | awk '{files+=$1; inserted+=$4; deleted+=$6; delta+=$4-$6; ratio=deleted/inserted} END {printf "Commit stats:\n- Files changed (total)..  %s\n- Lines added (total)....  %s\n- Lines deleted (total)..  %s\n- Total lines (delta)....  %s\n- Add./Del. ratio (1:n)..  1 : %s\n", files, inserted, deleted, delta, ratio }'
 
+echo
+echo "Contributors:"
+git shortlog -s -n -e
+
 exit $?
