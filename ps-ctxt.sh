788a2b2abb919ab96aeef4be7aef160d0bd5eb21
Robert W. Baumgartner <rwb@0x19e.net>
2019-04-28 16:12:07 -0500

    Increase column widths for ps-ctxt.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- ps-ctxt.sh
+++ ps-ctxt.sh
@@ -8,6 +8,6 @@ hash awk 2>/dev/null || { echo >&2 "You need to install gawk. Aborting."; exit 1
 pidstat -w 2 1 | grep Average | grep -v pidstat \
   | sort -r -n -k4 \
   | head -n 20 \
-  | awk '{ if ($3 != "PID") printf("PID:%-8s%-7scswch/s\t%-7snvcswch/s\t%s\n", $3, $4, $5, $6) }'
+  | awk '{ if ($3 != "PID") printf("PID:%-8s%-12scswch/s\t%-12snvcswch/s\t%s\n", $3, $4, $5, $6) }'
 
 exit $?
