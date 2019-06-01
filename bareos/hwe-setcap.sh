ecd4fe10e6962d400bface6c1dc0b846c1141d79
Robert W. Baumgartner <rwb@0x19e.net>
2019-05-31 23:38:31 -0500

    use full command paths in cron scripts; use /bin/bash instead of /bin/sh shell
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- bareos/hwe-setcap.sh
+++ bareos/hwe-setcap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Configures capabilities for Bareos LTO encryption plugin
 
-hash setcap 2>/dev/null || { echo >&2 "You need to install libcap2-bin. Aborting."; exit 1; }
+hash /sbin/setcap 2>/dev/null || { echo >&2 "You need to install libcap2-bin. Aborting."; exit 1; }
 
 if [ ! -e "/usr/sbin/bareos-sd" ]; then
   echo >&2 "ERROR: Missing bareos-sd command: /usr/sbin/bareos-sd"
@@ -12,15 +12,15 @@ if [ ! -e "/usr/sbin/bscrypto" ]; then
   exit 1
 fi
 
-setcap cap_sys_rawio=ep /usr/sbin/bareos-sd
-setcap cap_sys_rawio=ep /usr/sbin/bscrypto
+/sbin/setcap cap_sys_rawio=ep /usr/sbin/bareos-sd
+/sbin/setcap cap_sys_rawio=ep /usr/sbin/bscrypto
 
-if ! setcap -v cap_sys_rawio=ep /usr/sbin/bareos-sd; then
-  echo >&2 "ERROR: Failed to call setcap for /usr/sbin/bareos-sd"
+if ! /sbin/setcap -v cap_sys_rawio=ep /usr/sbin/bareos-sd; then
+  echo >&2 "ERROR: Failed to call /sbin/setcap for /usr/sbin/bareos-sd"
   exit 1
 fi
-if ! setcap -v cap_sys_rawio=ep /usr/sbin/bscrypto; then
-  echo >&2 "ERROR: Failed to call setcap for /usr/sbin/bscrypto"
+if ! /sbin/setcap -v cap_sys_rawio=ep /usr/sbin/bscrypto; then
+  echo >&2 "ERROR: Failed to call /sbin/setcap for /usr/sbin/bscrypto"
   exit 1
 fi
 
--- net/flush-iptables.sh
+++ net/flush-iptables.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # Resets iptables to default values.
 #
 
 # check if superuser
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" >&2
+   echo "This script must be run as root." >&2
    exit 1
 fi
 
