96d614efcd4b818bfbbcf1bd1744d903669ae945
Robert W. Baumgartner <rwb@0x19e.net>
2019-06-10 23:38:44 -0500

    Cleanup and correct sorting for mem-usage.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- mem-usage.sh
+++ mem-usage.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
+# Prints a table of top memory consuming processes
 
-ps --no-headers -eo user:20,pcpu,pmem | sort -k1,3n | awk '{num[$1]++; cpu[$1] += $2; mem[$1] += $3} END{printf("NPROC\t\t\tUSER\tCPU\tMEM\n"); for (user in cpu) printf("%d\t%20s\t%.2f\t%.2f\n", num[user], user, cpu[user], mem[user]) }'
+printf "NPROC\t\t\tUSER\tCPU\tMEM\n"
 
-#for pid in $(ps --no-headers -ef | awk '{print $2}'); do
-#    if [ -e /proc/$pid/smaps ]; then
-#      size=$(cat /proc/$pid/smaps 2>/dev/null | grep -m 1 -e ^Size: | awk '{print $2}')
-#      if [ -n "${size}" ]; then
-#        echo "* Mem usage for PID $pid"
-#        echo "-- Size: "$size
-#        echo "-- Rss:"
-#        cat /proc/$pid/smaps | grep -m 1 -e ^Rss: | awk '{print $2}'
-#        echo "-- Pss:"
-#        cat /proc/$pid/smaps | grep -m 1 -e ^Pss: | awk '{print $2}'
-#        echo "Shared Clean"
-#        cat /proc/$pid/smaps | grep -m 1 -e '^Shared_Clean:' | awk '{print $2}'
-#        echo "Shared Dirty"
-#        cat /proc/$pid/smaps | grep -m 1 -e '^Shared Dirty:' | awk '{print $2}'
-#      fi
-#    fi
-#done
+ps --no-headers -eo user:20,pcpu,pmem \
+  | awk '{num[$1]++; cpu[$1] += $2; mem[$1] += $3} END{for (user in cpu) printf("%d\t%20s\t%.2f\t%.2f\n", num[user], user, cpu[user], mem[user]) }' \
+  | sort -k4nr
+
+exit 0
