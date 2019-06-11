07d6b5f0e255f52823619ee7b8b4e91a20c86a42
Robert W. Baumgartner <rwb@0x19e.net>
2019-06-11 17:55:15 -0500

    Add displaytime() function to datetime helpers.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- helpers/datetime.sh
+++ helpers/datetime.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 # Date/time helpers
 
+function displaytime {
+  local T=$1
+  local D=$((T/60/60/24))
+  local H=$((T/60/60%24))
+  local M=$((T/60%60))
+  local S=$((T%60))
+  (( $D > 0 )) && printf '%d days ' $D
+  (( $H > 0 )) && printf '%d hours ' $H
+  (( $M > 0 )) && printf '%d minutes ' $M
+  (( $D > 0 || $H > 0 || $M > 0 )) && printf 'and '
+  printf '%d seconds\n' $S
+}
+
 function countdown(){
    start_date=$(($(date +%s) + $1));
    while [ "$start_date" -ge $(date +%s) ]; do
