cd1f94ae5953c63f207b42da8d1c88c3f671602a
Robert W. Baumgartner <rwb@0x19e.net>
2019-06-11 23:38:47 -0500

    Fix bug in gredit.sh; fix shellcheck warning in dump-info.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- dump-info.sh
+++ dump-info.sh
@@ -9,8 +9,8 @@
  uptime; \
  uname -a; \
  df -hT;\
- cat /proc/cpuinfo | grep "model name" |uniq -c; \
- cat /proc/meminfo | grep MemTotal |uniq -c; \
+ grep "model name" /proc/cpuinfo | uniq -c; \
+ grep MemTotal /proc/meminfo | uniq -c; \
  ifconfig | grep 'inet addr'; \
  echo; \
  wget --output-document=/dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip 2>&1 >/dev/null | grep --before-context=5 saved; \
