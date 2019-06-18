a2bae9265bd57f2be4780452b92e0c3dd234b3e4
Robert W. Baumgartner <rwb@0x19e.net>
2019-06-18 18:12:28 -0500

    Refactor net/get-wan-ip.sh to return IP of interface used to route 0.0.0.0 traffic
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- net/get-wan-ip.sh
+++ net/get-wan-ip.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 # Output current WAN IP
+# wan_iface=$(route -n | grep -P '^0\.0\.0\.0' | awk '{ print $8 }')
 
-ifconfig | grep 'inet addr:' \
-         | grep -v '127.0.0.1' \
+if ! wan_iface=$(route -n | grep -P '^0\.0\.0\.0\s+[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s+0\.0\.0\.0\s+[UG]{2}' | awk '{ print $8 }'); then
+  echo >&2 "ERROR: Failed to determine primary interface."
+  exit 1
+fi
+if [ -z "${wan_iface}" ]; then
+  echo >&2 "ERROR: Something went wrong trying to determine primary interface."
+  exit 1
+fi
+
+if ! ifconfig "${wan_iface}" \
+         | grep 'inet addr:' \
          | cut -d: -f2 \
-         | awk '{ print $1}'
+         | awk '{ print $1}'; then
+  echo >&2 "ERROR: Failed to resolve IP address for interface '${wan_iface}'."
+  exit 1
+fi
+
+exit 0
