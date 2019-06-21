884889030e94ff879bddd744b02031f82e3a8b27
Robert W. Baumgartner <rwb@0x19e.net>
2019-06-20 21:39:11 -0500

    Add scan-qr.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

---
+++ scan-qr.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Scan the desktop for a QR code and decode if found
+# Uses zbarimg, for example:
+#  zbarimg -q --raw "${temp_file}"
+
+hash gnome-screenshot 2>/dev/null || { echo >&2 "You need to install gnome-screenshot. Aborting."; exit 1; }
+hash zbarimg 2>/dev/null || { echo >&2 "You need to install zbar-tools. Aborting."; exit 1; }
+
+function countdown() {
+    start_date=$(($(date +%s) + $1));
+    while [ "$start_date" -ge "$(date +%s)" ]; do
+        seconds=$(date -u --date @$((start_date - $(date +%s))) +%s)
+        if [ "$seconds" -eq 0 ]; then
+            echo -ne "Taking screenshot ...........\r";
+            break;
+        else
+            echo -ne "Screenshot in $seconds second(s) ...\r";
+        fi
+        sleep 0.1
+    done
+}
+
+if ! temp_file=$(mktemp -t screenshot.XXXXXXXXXX)".png"; then
+  echo >&2 "ERROR: Failed to create temporary file for screenshot."
+  exit 1
+fi
+
+countdown 3
+
+if ! gnome-screenshot -f "${temp_file}"; then
+  echo >&2 "ERROR: Failed to take desktop screenshot."
+  exit 1
+fi
+
+echo -ne "Scanning for barcodes .......\r";
+
+err=0
+if ! zbarimg -q "${temp_file}"; then
+  err=1
+fi
+
+if [ -e "${temp_file}" ]; then
+  rm -f "${temp_file}"
+fi
+
+if [ "${err}" -ne 0 ]; then
+  exit 1
+fi
+
+exit 0
