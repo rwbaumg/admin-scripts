464caf5aab4510c52b2853f21a18ce4e40ac5b63
Robert W. Baumgartner <rwb@0x19e.net>
2019-06-26 01:08:59 -0500

    net: fix duplicate routes handling in get-wan-ip.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- net/get-wan-ip.sh
+++ net/get-wan-ip.sh
@@ -2,7 +2,7 @@
 # Output current WAN IP
 # wan_iface=$(route -n | grep -P '^0\.0\.0\.0' | awk '{ print $8 }')
 
-if ! wan_iface=$(route -n | grep -P '^0\.0\.0\.0\s+[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s+0\.0\.0\.0\s+[UG]{2}' | awk '{ print $8 }'); then
+if ! wan_iface=$(route -n | grep -P '^0\.0\.0\.0\s+[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s+0\.0\.0\.0\s+[UG]{2}' | awk '{ print $8 }' | uniq | head -n1); then
   echo >&2 "ERROR: Failed to determine primary interface."
   exit 1
 fi
