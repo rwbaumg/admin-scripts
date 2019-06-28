49467aac53fbbd261cf33937e581b34b0b2b9bf8
Robert W. Baumgartner <rwb@0x19e.net>
2019-06-27 22:09:35 -0500

    Fix APT commands in upgrade helper scripts.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- apt-dist-upgrade-chroots.sh
+++ apt-dist-upgrade-chroots.sh
@@ -6,5 +6,7 @@ hash schroot 2>/dev/null || { echo >&2 "You need to install schroot. Aborting.";
 
 for chroot in $(schroot -l|awk -F : '{print $2}'); do
     echo "Upgrading '$chroot' chroot ..."
-    schroot -q -c "$chroot" -u root -- sh -c 'apt-get -qq update && apt-get -qy dist-upgrade && apt-get clean'
+    schroot -q -c "$chroot" -u root -- sh -c 'apt-get -qq update && apt-get -qy dist-upgrade && apt-get -qy autoremove && apt-get clean'
 done
+
+exit 0
--- apt-dist-upgrade.sh
+++ apt-dist-upgrade.sh
@@ -8,10 +8,16 @@ hash apt-get 2>/dev/null || { echo >&2 "You need to install apt. Aborting."; exi
 
 BASE_COMMAND="sudo apt-get $*"
 
-if ! ${BASE_COMMAND} update && \
-     ${BASE_COMMAND} dist-upgrade && \
-     ${BASE_COMMAND} autoremove; then
+if ! ${BASE_COMMAND} update; then
   echo >&2 "ERROR: System update failed."
+  exit 1
+fi
+if ! ${BASE_COMMAND} dist-upgrade; then
+  echo >&2 "ERROR: System upgrade failed."
+  exit 1
+fi
+if ! ${BASE_COMMAND} autoremove; then
+  echo >&2 "ERROR: Package autoremoval failed."
   exit 1
 fi
 
