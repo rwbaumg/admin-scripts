2674959daed00c34b6ae34c8ad5a4816af31262e
Robert W. Baumgartner <rwb@0x19e.net>
2019-08-12 22:26:56 -0500

    Add keygen script for Supermicro IPMI/OOB management.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

---
+++ setup/sm-ipmi-keygen.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Supermicro IPMI key generator
+# See https://peterkleissner.com/2018/05/27/reverse-engineering-supermicro-ipmi/
+
+echo "Supermicro IPMI/OOB management key generator"
+
+# TODO: Validate input MAC
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <ipmi_mac>" >&2
+  exit 1
+fi
+
+MAC=$(echo "${1}" | awk '{ print toupper($0) }')
+MAC=${MAC//[^0-9A-F]/}
+
+if ! key=$(echo -n '${MAC}' \
+            | xxd -r -p \
+            | openssl dgst -sha1 -mac HMAC -macopt hexkey:8544E3B47ECA58F9583043F8 \
+            | awk '{print $2}' \
+            | cut -c 1-24 \
+            | sed 's/.\{4\}/&\-/g;s/\-$//'); then
+  echo "ERROR: Failed to generate IPMI key." >&2
+  exit 1
+fi
+
+echo "IPMI MAC address : ${MAC}"
+echo "IPMI license key : ${key}"
+
+exit 0
