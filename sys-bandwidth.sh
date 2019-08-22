6410a965b3f900962d454dc669a75f4c0c16a927
Robert W. Baumgartner <rwb@0x19e.net>
2019-08-21 22:23:24 -0500

    cleanup sys-bandwidth.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- sys-bandwidth.sh
+++ sys-bandwidth.sh
@@ -2,4 +2,9 @@
 # read 32GB zero's and throw them away.
 # rwb[at]0x19e[dot]net
 
-dd if=/dev/zero of=/dev/null bs=1M count=$((32*1024))
+SIZE_MB=32
+
+dd if=/dev/zero of=/dev/null bs=1M count=$((SIZE_MB*1024))
+# dd if=/dev/zero bs=1M count=$((SIZE_MB*1024)) | pv -brtep -s "${SIZE_MB}m" | dd of=/dev/null
+
+exit 0
