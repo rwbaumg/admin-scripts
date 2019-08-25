4efa0afd550ca36c10769a3232dd89325e845e13
Robert W. Baumgartner <rwb@0x19e.net>
2019-08-24 19:50:45 -0500

    Add get-src-main.sh script for reading main() from C source files.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

---
+++ get-src-main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Get the main function from the specified C source file
+
+if [ -n "$*" ]; then
+  if ! MATCH=$(grep -Pzo "(?s)(\s*)\N([a-z]+(\s)?){0,3}main.*?{.*?\1}" $@); then
+    exit 1
+  fi
+else
+  if ! MATCH=$(grep -Pzo "(?s)(\s*)\N([a-z]+(\s)?){0,3}main.*?{.*?\1}" *.c); then
+    exit 1
+  fi
+fi
+
+if [ -z "$(echo "${MATCH}" | head -n1)" ]; then
+  MATCH=$(echo "${MATCH}" | tail -n+2)
+fi
+
+echo "${MATCH}"
+
+exit 0
