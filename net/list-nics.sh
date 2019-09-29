925b5a79cc99500e03d8888f236e76123d95904e
Robert W. Baumgartner <rwb@0x19e.net>
2019-09-29 01:52:14 -0500

    net: add optional filtering support to list-nics.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- net/list-nics.sh
+++ net/list-nics.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 # List Network Interface Device(s) excluding 'lo' and 'vif.*' interfaces
 
+# Set the path to the network device file
 DEV_FILE="/proc/net/dev"
+
+# Define a RegEx to filter output NICs
+# DEV_FILTER="(lo|vif)"
+DEV_FILTER="(lo)"
+
+# Define a RegEx to filter output NICs
+DEV_SELECT=""
+if [ -n "$1" ]; then
+  DEV_SELECT="$1"
+fi
+
 if [ ! -e "${DEV_FILE}" ]; then
   echo >&2 "ERROR: File does not exist: ${DEV_FILE}"
   exit 1
@@ -13,41 +25,53 @@ if ! NICS=$(cat "${DEV_FILE}" \
               | tr -d ' ' \
               | sort -h \
               | uniq \
-              | grep -vE '(lo|vif)'); then
+              | grep -vE "${DEV_FILTER}"); then
   echo >&2 "ERROR: Failed to determine local Network Interface Card(s) (NICs)."
   exit 1
 fi
 
-echo "${NICS}" | while read -r dev; do
-  dev_path="/sys/class/net/${dev}/operstate"
-  if [ ! -e "${dev_path}" ]; then
-    echo >&2 "ERROR: Device file not found: ${dev_path}"
+function list_nics() {
+  echo "${NICS}" | while read -r dev; do
+    dev_path="/sys/class/net/${dev}/operstate"
+    if [ ! -e "${dev_path}" ]; then
+      echo >&2 "ERROR: Device file not found: ${dev_path}"
+      exit 1
+    fi
+
+    dev_up=0
+    dev_state=$(cat "${dev_path}")
+    case "${dev_state}" in
+      down)
+        dev_up=0
+        shift
+      ;;
+      up)
+        dev_up=1
+        shift
+      ;;
+      unknown)
+        dev_up=-1
+        shift
+      ;;
+      *)
+        # unknown state
+        echo >&2 "ERROR: Device '${dev}' state '${dev_state}' is not supported."
+        exit 1
+      ;;
+    esac
+
+    printf "$COL_RESET%-24s : %s\n" "${dev}" "${dev_state}"
+  done
+}
+
+if [ -n "${DEV_SELECT}" ]; then
+  if ! list_nics | sort -t: -k2 | grep -Ei "${DEV_SELECT}"; then
     exit 1
   fi
-
-  dev_up=0
-  dev_state=$(cat "${dev_path}")
-  case "${dev_state}" in
-    down)
-      dev_up=0
-      shift
-    ;;
-    up)
-      dev_up=1
-      shift
-    ;;
-    unknown)
-      dev_up=-1
-      shift
-    ;;
-    *)
-      # unknown state
-      echo >&2 "ERROR: Device '${dev}' state '${dev_state}' is not supported."
-      exit 1
-    ;;
-  esac
-
-  printf "$COL_RESET%-24s : %s\n" "${dev}" "${dev_state}"
-done
+else
+  if ! list_nics | sort -t: -k2; then
+    exit 1
+  fi
+fi
 
 exit 0
