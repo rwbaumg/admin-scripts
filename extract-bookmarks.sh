ba6cb1fbd473fde80d2571e9086e5b3afa17d83f
Robert W. Baumgartner <rwb@0x19e.net>
2019-11-16 23:28:07 -0600

    Add additional output formats to extract-bookmarks.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- extract-bookmarks.sh
+++ extract-bookmarks.sh
@@ -2,6 +2,11 @@
 # Extract URLs from an HTML document, sort and write unique entries in CSV format.
 # This script can be used to collate multiple exported browser bookmarks backup files.
 
+# FORMAT="csv"
+# FORMAT="trac-links"
+# FORMAT="trac-table"
+# FORMAT="md-links"
+
 function usage() {
         echo >&2 "Usage: $0 <bookmarks_html_file>..."
 }
@@ -27,16 +32,59 @@ function create_csv() {
         done
 }
 
+function create_trac_links() {
+        if [ -z "$1" ]; then
+                usage
+                exit 1
+        fi
+        for f in "$@"; do
+                if [ ! -e "$f" ]; then
+                        echo >&2 "ERROR: Failed to locate HTML file(s): $*"
+                        exit 1
+                fi
+        done
+
+        cat "$@" | while read -r line; do
+                link=$(echo "${line}" | grep -Pio '(?<=HREF\=\")[^\"]+(?=\")')
+                desc=$(echo "${line}" | grep -Pio '(?<=\"\>)[^\"]+(?=\<\/A\>)')
+                if [ ! -z "${link}" ]; then
+                        echo "- [${link// /\%20/} ${desc}]"
+                fi
+        done
+}
+
 if [ -z "$1" ]; then
         usage
         exit 1
 fi
 
-if ! CSV=$(create_csv "$@"); then
-        echo >&2 "ERROR: Failed to create CSV file."
-        exit 1
+if [ -z "${FORMAT+set}" ]; then
+	export FORMAT="csv"
+fi
+if [ -z "${FORMAT}" ]; then
+	echo >&2 "ERROR: Output format not specified."
+	exit 1
 fi
 
-echo "${CSV}" | sort | uniq
+case "$FORMAT" in
+	default|csv)
+		if ! OUTPUT=$(create_csv "$@"); then
+			echo >&2 "ERROR: Failed to create CSV file."
+			exit 1
+		fi
+	;;
+	trac|trac-links)
+		if ! OUTPUT=$(create_trac_links "$@"); then
+			echo >&2 "ERROR: Failed to create Trac links markup."
+			exit 1
+		fi
+	;;
+	*)
+		echo >&2 "ERROR: Unsupported output format '${FORMAT}'."
+		exit 1
+	;;
+esac
+
+echo "${OUTPUT}" | sort | uniq
 
 exit 0
