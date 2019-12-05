50621bccb4906220a0a6cac4d3adb972eeceedf5
Robert W. Baumgartner <rwb@0x19e.net>
2019-12-05 01:44:09 -0600

    net: add gen-ipv6-ula.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

---
+++ net/gen-ipv6-ula.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+r1=$(dd if=/dev/urandom bs=1 count=1 2>/dev/null | hexdump -e '1/1 "%02x"')
+r2=$(dd if=/dev/urandom bs=2 count=1 2>/dev/null | hexdump -e '2/1 "%02x"')
+r3=$(dd if=/dev/urandom bs=2 count=1 2>/dev/null | hexdump -e '2/1 "%02x"')
+
+echo "ULA prefix: fd$r1:$r2:$r3::/48"
+
+exit 0
