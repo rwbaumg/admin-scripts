bbacc6fb25349cb7336dab8aaf2f4ddd05bcfd9c
Robert W. Baumgartner <rwb@0x19e.net>
2019-12-08 20:34:00 -0600

    add comment to list-mountpoints.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- list-mountpoints.sh
+++ list-mountpoints.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Lists partitions on the current system
+#
+# NOTE: To remount a given filesystem, for example to enable RW:
+#            sudo mount -o remount,rw /partition/identifier /mount/point
 
 if ! info=$(sudo mount -v | grep "^/" | awk '{print "Partition identifier: " $1  "\n Mountpoint: "  $3 "\n"}'); then
   echo >&2 "ERROR: Failed to get mountpoint info."
