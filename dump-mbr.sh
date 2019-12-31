d12dbaa8d9706f616f2ec9832016673c0e1dc36f
Robert W. Baumgartner <rwb@0x19e.net>
2019-12-30 18:20:52 -0600

    Add dump-mbr.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

---
+++ dump-mbr.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+hash xxd 2>/dev/null || { echo >&2 "You need to install xxd. Aborting."; exit 1; }
+hash dd 2>/dev/null || { echo >&2 "You need to install dd. Aborting."; exit 1; }
+
+# check if superuser
+if [[ $EUID -ne 0 ]]; then
+   echo >&2 "This script must be run as root."
+   exit 1
+fi
+
+if [ ! -z "$1" ]; then
+  DEVICE="$1"
+fi
+
+if [ -z "${DEVICE}" ]; then
+  DEVICE="/dev/sda"
+fi
+
+if [ ! -b "${DEVICE}" ]; then
+  echo >&2 "ERROR: ${DEVICE} is not a valid block device."
+  exit 1
+fi
+
+# dump MBR from device
+echo "Dumping MBR from device ${DEVICE} ..."
+dd if=/dev/sda bs=512 count=1 | xxd
+echo "[ EOF ]"
+
+exit 0
