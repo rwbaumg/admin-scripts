3f8a3abcdbbcf79541e22b8228041f71bc0b1507
Robert W. Baumgartner <rwb@0x19e.net>
2020-02-09 00:32:21 -0600

    bareos: modify insall-client.sh to always use /usr/bin/gpg; fix etckeeper handling.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- bareos/install-client.sh
+++ bareos/install-client.sh
@@ -11,7 +11,7 @@ hash grep 2>/dev/null || { echo >&2 "You need to install grep. Aborting."; exit 
 hash awk 2>/dev/null || { echo >&2 "You need to install awk. Aborting."; exit 1; }
 hash sudo 2>/dev/null || { echo >&2 "You need to install sudo. Aborting."; exit 1; }
 hash wget 2>/dev/null || { echo >&2 "You need to install wget. Aborting."; exit 1; }
-hash gpg 2>/dev/null || { echo >&2 "You need to install gnupg. Aborting."; exit 1; }
+hash /usr/bin/gpg 2>/dev/null || { echo >&2 "You need to install gnupg. Aborting."; exit 1; }
 hash apt-get 2>/dev/null || { echo >&2 "You need to install apt. Aborting."; exit 1; }
 hash tee 2>/dev/null || { echo >&2 "You need to install tee. Aborting."; exit 1; }
 hash lsb_release 2>/dev/null || { echo >&2 "You need to install lsb-release. Aborting."; exit 1; }
@@ -150,17 +150,12 @@ check_protocol()
 
 check_etckeeper()
 {
-  if [[ $EUID -ne 0 ]]; then
-    echo >&2 "WARNING: Must run as root to commit /etc changes."
-    return
-  fi
-
   # git handling for etckeeper (check if /etc/.git exists)
   if [ -d /etc/.git  ] && hash git 2>/dev/null; then
-    if git -C "/etc" rev-parse > /dev/null 2>&1; then
+    if sudo git -C "/etc" rev-parse > /dev/null 2>&1; then
       # check /etc/apt for modifications
       # if there are changes, commit them
-      if [[ "$(git --git-dir=/etc/.git --work-tree=/etc status --porcelain -- /etc/apt | grep -E '^(M| M)')" != "" ]]; then
+      if [[ "$(sudo git --git-dir=/etc/.git --work-tree=/etc status --porcelain -- /etc/apt | grep -E '^(M| M)')" != "" ]]; then
         if [ "${ETCKEEPER_COMMIT}" != "true" ]; then
           echo >&2 "WARNING: Uncommitted changes under version control: /etc/apt"
           echo >&2 "WARNING: You may want to enable automatic handling with --enable-etckeeper"
@@ -185,41 +180,45 @@ install_key_from_url()
   fi
 
   # check if the key is already installed
-  KEY_RW=$(wget -qO - "${KEY_URL}")
+  echo "Downloading release signing key from '${key_url}' ..."
+  KEY_RW=$(wget -qO - "${key_url}")
   if [ -z "${KEY_RW}" ]; then
-    echo >&2 "Failed to retrieve signing key from ${KEY_URL}"
+    echo >&2 "Failed to retrieve signing key from ${key_url}"
     exit 1
   fi
-  if ! echo "${KEY_RW}" | gpg --list-packets > /dev/null 2>&1; then
-    echo >&2 "Invalid key returned from URL ${KEY_URL}"
+  if ! echo "${KEY_RW}" | /usr/bin/gpg --list-packets > /dev/null 2>&1; then
+    echo >&2 "Invalid key returned from URL ${key_url}"
     exit 1
   fi
 
-  GPG_RW=$(echo "${KEY_RW}" | gpg --with-fingerprint --keyid-format SHORT 2>/dev/null | grep -P '^pub' | head -n1)
+  GPG_RW=$(echo "${KEY_RW}" | /usr/bin/gpg --with-fingerprint --keyid-format SHORT 2>/dev/null | grep -P '^pub' | head -n1)
+
+  # Get key name
   KEY_ID=$(echo "${GPG_RW}" | cut -d' ' -f5- | awk '{$1=$1};1')
 
+  # Get key size/fingerprint
   KEY_TP=$(echo "${GPG_RW}" | awk '{ print $2 }' | awk '{$1=$1};1')
+
+  # Get key size and type
   KEY_SZ=$(echo "${KEY_TP}" | cut -d/ -f1)
+
+  # Get key fingerprint
   KEY_FP=$(echo "${KEY_TP}" | cut -d/ -f2)
+
+  echo "Found signing key  : ${KEY_ID}"
+  if [ $VERBOSITY -gt 0 ]; then
+  echo "Key fingerprint    : ${KEY_FP}"
+  echo "Key size and type  : ${KEY_SZ}"
+  fi
 
   KEY_LIST=$(apt-key list --keyid-format SHORT 2>/dev/null)
   if echo "${KEY_LIST}" | grep "${KEY_FP}" > /dev/null 2>&1; then
-    echo "Found signing key  : ${KEY_ID}"
-    if [ $VERBOSITY -gt 0 ]; then
-    echo "Key fingerprint    : ${KEY_FP}"
-    echo "Key size and type  : ${KEY_SZ}"
-    fi
+    echo "Key '${KEY_ID}' (${KEY_TP}) is already installed."
     return
   fi
 
   # add the release key
-  echo "Retrieve signing key from ${KEY_URL} ..."
-
-  if [ $VERBOSITY -gt 0 ]; then
-  echo "Key identifier     : ${KEY_ID}"
-  echo "Key fingerprint    : ${KEY_FP}"
-  echo "Key size and type  : ${KEY_SZ}"
-  fi
+  echo "Installing '${KEY_ID}' (${KEY_TP}) ..."
 
   if ! echo "${KEY_RW}" | sudo apt-key add -; then
     exit 1
