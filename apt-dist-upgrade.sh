f53d894476cf0bf67cd2d3a4d4cf7ab42c1503c3
Robert W. Baumgartner <rwb@0x19e.net>
2020-02-18 18:11:55 -0600

    Update sudo/root permission checks for apt scripts.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- apt-dist-upgrade.sh
+++ apt-dist-upgrade.sh
@@ -8,6 +8,15 @@ hash apt 2>/dev/null || { echo >&2 "You need to install apt. Aborting."; exit 1;
 
 BASE_COMMAND="sudo apt-get $*"
 
+# Ensure sudo privileges for the current user if not running as root.
+if [[ $EUID -ne 0 ]]; then
+  echo "NOTICE: Running as user $USER; sudo privileges required."
+  if ! sudo echo "" > /dev/null 2>&1; then
+    echo >&2 "ERROR: Must have sudo privileges to modify configuration files."
+    exit 1
+  fi
+fi
+
 if ! ${BASE_COMMAND} update; then
   echo >&2 "ERROR: System update failed."
   exit 1
