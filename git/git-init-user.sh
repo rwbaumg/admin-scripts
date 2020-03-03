aeb4f3722f4119b5e33743224f9935ca9b2bb70a
Robert W. Baumgartner <rwb@0x19e.net>
2020-03-03 02:12:42 -0600

    git: add additional options to git-init-user.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- git/git-init-user.sh
+++ git/git-init-user.sh
@@ -45,6 +45,9 @@ usage()
     OPTIONS
 
      -n, --full-name <arg>  Specify the user's full name.
+     -e, --email <arg>      Specify the user's e-mail address.
+
+     --no-install           Do not install missing diff support packages.
 
      -f, --force            Overwrite existing user configuration.
      -v, --verbose          Make the script more verbose.
@@ -72,17 +75,36 @@ test_arg()
     fi
 }
 
+test_email_arg()
+{
+  # test user argument
+  local arg="$1"
+  local argv="$2"
+
+  test_arg "$arg" "$argv"
+
+  if [ -z "$argv" ]; then
+    argv="$arg"
+  fi
+
+  # regex borrowed from https://emailregex.com/
+  re='(?:[a-z0-9!#$%&'"'"'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"'"'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])'
+  if ! echo "$argv" | grep -Poq "$re"; then
+    usage "Invalid e-mail address: ${argv}"
+  fi
+}
+
+NO_INSTALL=0
 FORCE="false"
 VERBOSITY=0
 
-#VERBOSE=""
-#check_verbose()
-#{
-#  if [ $VERBOSITY -gt 1 ]; then
-#    VERBOSE="-v"
-#  fi
-#}
-
+VERBOSE=""
+check_verbose()
+{
+  if [ $VERBOSITY -gt 1 ]; then
+    VERBOSE="-v"
+  fi
+}
 
 # process arguments
 #[ $# -gt 0 ] || usage
@@ -94,8 +116,18 @@ while [ $# -gt 0 ]; do
       USER_FULLNAME="$1"
       shift
     ;;
+    -e|--email)
+      test_email_arg "$1" "$2"
+      shift
+      USER_EMAIL="$1"
+      shift
+    ;;
     -f|--force)
       FORCE="true"
+      shift
+    ;;
+    --no-install)
+      NO_INSTALL=1
       shift
     ;;
     -v|--verbose)
@@ -136,11 +168,11 @@ if [ -e "$HOME/.gitconfig" ] && [ "${FORCE}" != "true" ]; then
 else
   if [ -e "$HOME/.gitconfig" ]; then
     echo >&2 "WARNING: '$HOME/.gitconfig' already exists; creating backup ..."
-    cp -v "$HOME/.gitconfig" "$HOME/.gitconfig.backup-$(date '+%Y%m%d')"
+    cp ${VERBOSE} "$HOME/.gitconfig" "$HOME/.gitconfig.backup-$(date '+%Y%m%d')"
   fi
 
   echo "Installing .gitconfig ..."
-  cp -v "${ROOT_DIR}/gitconfig.template" "${HOME}/.gitconfig"
+  cp ${VERBOSE} "${ROOT_DIR}/gitconfig.template" "${HOME}/.gitconfig"
 
   err=0
   if [ -z "${USER_FULLNAME}" ]; then
@@ -157,16 +189,22 @@ else
     err=1; echo >&2 "WARNING: Failed to determine current user's name; cannot configure user.name property."
   fi
 
-  if ! USER_DOMAIN=$(hostname -d); then
-    echo >&2 "ERROR: Local hostname does not appear to have a configured domain name (hostname -d)."
-  fi
-  if [ -n "${USER_DOMAIN}" ]; then
-    USER_EMAIL="${USER}@${USER_DOMAIN}"
+  if [ -z "${USER_EMAIL}" ]; then
+    if ! USER_DOMAIN=$(hostname -d); then
+      echo >&2 "ERROR: Local hostname does not appear to have a configured domain name (hostname -d)."
+    fi
+    if [ -n "${USER_DOMAIN}" ]; then
+      USER_EMAIL="${USER}@${USER_DOMAIN}"
+      if ! git config --global user.email "${USER_EMAIL}"; then
+        err=1; echo >&2 "WARNING: Failed to set global user email."
+      fi
+    else
+      err=1; echo >&2 "WARNING: Failed to determine local domain name; cannot configure user e-mail."
+    fi
+  else
     if ! git config --global user.email "${USER_EMAIL}"; then
       err=1; echo >&2 "WARNING: Failed to set global user email."
     fi
-  else
-    err=1; echo >&2 "WARNING: Failed to determine local domain name; cannot configure user e-mail."
   fi
 
   if [ "${err}" -ne 1 ]; then
@@ -180,11 +218,11 @@ if [ -e "$HOME/.gitattributes" ] && [ "${FORCE}" != "true" ]; then
 else
   if [ -e "$HOME/.gitattributes" ]; then
     echo >&2 "WARNING: '$HOME/.gitattributes' already exists; creating backup ..."
-    cp -v "$HOME/.gitattributes" "$HOME/.gitattributes.backup-$(date '+%Y%m%d')"
+    cp ${VERBOSE} "$HOME/.gitattributes" "$HOME/.gitattributes.backup-$(date '+%Y%m%d')"
   fi
 
   echo "Installing .gitattributes ..."
-  cp -v "${ROOT_DIR}/gitattributes.template" "${HOME}/.gitattributes"
+  cp ${VERBOSE} "${ROOT_DIR}/gitattributes.template" "${HOME}/.gitattributes"
 fi
 
 # Check for missing diff support
@@ -245,17 +283,36 @@ done
 if [ "${#missing[@]}" -gt 0 ]; then
   echo "Found ${#missing[@]} missing package(s)."
 
+  if [ "${NO_INSTALL}" == "1" ]; then
+    echo "INFO: Skipped installing packages for extended diff support."
+    echo "To install missing packages using apt, run:"
+    echo "sudo apt install ${packages}"
+    exit 0
+  fi
+
   # See if apt-get is available
-  if hash apt-get 2>/dev/null; then
-    echo "Attempting to install via apt-get ..."
-    apt_command="sudo apt-get install $packages"
+  if hash apt 2>/dev/null; then
+    # Ensure sudo privileges for the current user if not running as root.
+    echo "Validating permissions for user $USER ..."
+    if [[ $EUID -ne 0 ]]; then
+      echo "NOTICE: Running as user $USER; sudo privileges required."
+      hash sudo 2>/dev/null || { echo >&2 "You need to install sudo. Aborting."; exit 1; }
+      if ! sudo echo "" > /dev/null 2>&1; then
+        echo >&2 "ERROR: Must have sudo privileges to modify configuration files."
+        exit 1
+      fi
+    fi
+
+    echo "Attempting to install: $packages ..."
+    apt_command="sudo apt install $packages"
     if ! ${apt_command}; then
       echo >&2 "ERROR: Failed to install missing packages."
       exit 1
     fi
   else
-    echo "For example, to install missing packages using apt-get run:"
-    echo "sudo apt-get install ${packages}"
+    echo "WARNING: Could not install missing packages for extended diff support."
+    echo "To install missing packages using apt, run:"
+    echo "sudo apt install ${packages}"
   fi
 fi
 
