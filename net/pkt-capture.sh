9f31d89cf6da96b81db76327d975b211d8850e5d
Robert W. Baumgartner <rwb@0x19e.net>
2020-04-08 20:17:00 -0500

    net: update pkt-capture.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- net/pkt-capture.sh
+++ net/pkt-capture.sh
@@ -4,7 +4,21 @@
 
 hash tcpdump 2>/dev/null || { echo >&2 "You need to install tcpdump. Aborting."; exit 1; }
 
-OPTS="-nnXSs 1514"
+# Specify capture options
+# -i  : Specify the interface(s) to capture from, eg. -i eth0
+# -e  : Get ethernet header as well
+# -n  : Do not resolve hostnames
+# -nn : Do not resolve hostnames OR ports
+# -s0 : Snap length - 0=unlimited
+# -S  : Print absolute sequence numbers
+# -X  : Include both hex AND ascii output
+# -XX : Same as -X but includes ethernet header
+# -v  : Increase packet detail - can be -v, -vv or -vvv
+# -w  : Write output to file - eg, -w file.pcap
+#
+# OPTS="-nnXSs 1514"
+# OPTS="-nnvvvXSs 1514"
+OPTS="-nnvvveXXSs0"
 
 if [[ -z "$1" ]]; then
   echo "Usage: $0 <iface> [file]"
@@ -18,4 +32,10 @@ else
 fi
 
 echo "Capturing $1 to '$OUTPUT' ..."
-tcpdump "$OPTS" -w "$OUTPUT" -i "$1"
+echo "Using options: ${OPTS}"
+
+if ! tcpdump "$OPTS" -w "$OUTPUT" -i "$1"; then
+  exit 1
+fi
+
+exit 0
