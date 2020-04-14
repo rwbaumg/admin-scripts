6ce2affed94b89c555982d2003c051618f4e6b8b
Robert W. Baumgartner <rwb@0x19e.net>
2020-04-14 01:10:09 -0500

    Update dump-info.sh speedtest server.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- dump-info.sh
+++ dump-info.sh
@@ -2,6 +2,10 @@
 # get some info about the current host in a nifty one-liner
 # rwb[at]0x19e.net
 
+#SPEEDTEST_URL="http://speedtest.tele2.net/10MB.zip"
+#SPEEDTEST_URL="http://speedtest.wdc01.softlayer.com/downloads/test10.zip"
+SPEEDTEST_URL="http://chicago02.speedtest.windstream.net:8080/speedtest/random2500x2500.jpg"
+
 (printf "%s - %s\n" "$(date)" "$(hostname --fqdn)"; \
  uname -a; echo; \
  uptime; \
@@ -12,7 +16,7 @@
  grep MemTotal /proc/meminfo | uniq -c; \
  ifconfig | grep 'inet addr'; \
  echo; \
- wget --output-document=/dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip /dev/null 2>&1 | grep --before-context=5 saved; \
+ wget --output-document=/dev/null "${SPEEDTEST_URL}" /dev/null 2>&1 | grep --before-context=5 saved; \
  echo; \
  if hash finger 2>/dev/null; then finger root; fi; \
  lsof -i) | uniq
