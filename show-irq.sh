89b3c6e13e81e377017e930667e00b96e8c375b9
Robert W. Baumgartner <rwb@0x19e.net>
2020-06-18 00:30:14 -0500

    Fix bug in show-irq.sh script /proc/interrupts parsing.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- show-irq.sh
+++ show-irq.sh
@@ -3,13 +3,17 @@
 
 IRQ_REQ="$1"
 
-if ! OUTPUT=$(cat /proc/interrupts | tail -n+2 | awk '{ printf "IRQ %s %s %s %s\n", $1, $(NF - 2), $(NF - 1), $(NF) }'); then
+if ! RAW_OUTPUT=$(grep -vP '^(ERR|MIS)' /proc/interrupts | tail -n+2 | awk '{printf "%s ", $1; for(j=(NF-2);j<=NF;j++){if($j~/^[A-Za-z]/){printf "%s ", $j} }; if($j=NF){printf "\n";} }' && grep -P '^(ERR|MIS)' /proc/interrupts | awk '{ printf "%s %s\n", $1, $2}'); then
   echo >&2 "ERROR: Failed to read interrupts."
+  exit 1
+fi
+if ! OUTPUT=$(echo "${RAW_OUTPUT}" | awk -F: '{ printf "IRQ %s\t: %s\n", $1, $2 }'); then
+  echo >&2 "ERROR: Failed to parse interrupts."
   exit 1
 fi
 
 if [ ! -z "${IRQ_REQ}" ]; then
-  if ! echo "${OUTPUT}" | grep -P "^(IRQ(\s)?)?${IRQ_REQ}\:"; then
+  if ! echo "${OUTPUT}" | grep -P "^(IRQ(\s+)?)?${IRQ_REQ}"; then
     echo "ERROR: Could not find interrupt '${IRQ_REQ}'."
   fi
 else
