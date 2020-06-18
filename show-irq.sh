ff5ca62e5253f890560a17fe6ad93c7d3b2c4240
Robert W. Baumgartner <rwb@0x19e.net>
2020-06-17 23:38:44 -0500

    Fix show-irq.sh script column handling.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- show-irq.sh
+++ show-irq.sh
@@ -3,13 +3,15 @@
 
 IRQ_REQ="$1"
 
-if ! OUTPUT=$(cat /proc/interrupts | tail -n+2 | awk '{ printf "IRQ %s %s %s %s\n", $1, $90, $91, $92 }'); then
+if ! OUTPUT=$(cat /proc/interrupts | tail -n+2 | awk '{ printf "IRQ %s %s %s %s\n", $1, $(NF - 2), $(NF - 1), $(NF) }'); then
   echo >&2 "ERROR: Failed to read interrupts."
   exit 1
 fi
 
 if [ ! -z "${IRQ_REQ}" ]; then
-  echo "${OUTPUT}" | grep -P "^(IRQ(\s)?)?${IRQ_REQ}\:"
+  if ! echo "${OUTPUT}" | grep -P "^(IRQ(\s)?)?${IRQ_REQ}\:"; then
+    echo "ERROR: Could not find interrupt '${IRQ_REQ}'."
+  fi
 else
   echo "${OUTPUT}"
 fi
