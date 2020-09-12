269c24ad300c5fc2bb78357227da90372e479e1c
Robert W. Baumgartner <rwb@0x19e.net>
2020-09-11 20:01:39 -0500

    Add sorting to lstty.sh script output.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- lstty.sh
+++ lstty.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 # List available serial devices and driver information
 
-for f in /sys/class/tty/*/device/driver; do
+hash grep 2>/dev/null || { echo >&2 "You need to install grep. Aborting."; exit 1; }
+hash sort 2>/dev/null || { echo >&2 "You need to install sort. Aborting."; exit 1; }
 
-  driver_dir="$f"
-  serial_dev=$(echo "$f" | grep -Po '(?<=tty\/)[^\/]+(?=\/device)')
-  driver_dir=$(readlink -f "$f")
-  echo -e "$serial_dev \t -> $driver_dir"
+function get_devices() {
+  for f in /sys/class/tty/*/device/driver; do
 
-done
+    driver_dir="$f"
+    serial_dev=$(echo "$f" | grep -Po '(?<=tty\/)[^\/]+(?=\/device)')
+    driver_dir=$(readlink -f "$f")
+    echo -e "$serial_dev \t -> $driver_dir"
+
+  done
+}
+
+OUTPUT=$(get_devices)
+
+echo "${OUTPUT}" | sort -V
 
 exit 0
