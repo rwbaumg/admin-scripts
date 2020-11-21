96be8161217d14a34befec7eaa9a648102c679f8
Robert W. Baumgartner <rwb@0x19e.net>
2020-11-20 21:22:07 -0600

    Fix bugs in burn-iso.sh script.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- burn-iso.sh
+++ burn-iso.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# creates an image of a CD/DVD disk
+# burns an iso image to a CD/DVD disc
 # rwb[at]0x19e.net
 
 # specify the CD/DVD drive
@@ -8,13 +8,18 @@ IMAGE=""
 
 hash isoinfo 2>/dev/null || { echo >&2 "You need to install genisoimage. Aborting."; exit 1; }
 hash wodim 2>/dev/null || { echo >&2 "You need to install wodim. Aborting."; exit 1; }
+hash setcd 2>/dev/null || { echo >&2 "You need to install setcd. Aborting."; exit 1; }
+hash growisofs 2>/dev/null || { echo >&2 "You need to install growisofs. Aborting."; exit 1; }
 
 if [ -n "${1}" ]; then
   if [ "${1}" == "-h" ] || [ "${1}" == "--help" ]; then
-    echo "Usage: $0 [iso] [drive]"
+    echo "Usage: $0 <iso> [drive]"
     exit 1
   fi
   IMAGE="${1}"
+elif [ -z "${1}" ]; then
+  echo "Usage: $0 <iso> [drive]"
+  exit 1
 fi
 if [ -n "${2}" ]; then
   DRIVE="${2}"
@@ -35,12 +40,6 @@ if [ $EUID -eq 0 ]; then
    exit 1
 fi
 
-# check if a disk is inserted
-if ! blkid "$DRIVE" > /dev/null 2>&1; then
-  echo >&2 "ERROR: No disk found in $DRIVE"
-  exit 1
-fi
-
 if ! SIZE=$(stat --format="%s" "$IMAGE"); then
   echo >&2 "ERROR: Failed to determine block size of image ${IMAGE}"
   exit 1
@@ -58,17 +57,48 @@ if [ -z "${IMAGE_NAME}" ]; then
   echo >&2 "WARNING: Failed to determine image name; using '${IMAGE_NAME}'."
 fi
 
+echo "Checking for disc ..."
+while true; do
+  cdinfo=$(setcd -i "$DRIVE")
+  case "$cdinfo" in
+    *'Disc found'*)
+      break;
+      ;;
+    *'not ready'*)
+      echo '(waiting for drive to be ready)' >&2;
+      sleep 3;
+      ;;
+    *'is open'*)
+      echo '(drive is open)' >&2;
+      sleep 5;
+      ;;
+    *)
+      printf 'Confused by setcd -i, bailing out:\n%s\n' "$cdinfo" &2
+      exit 1
+    esac
+done
+
 # get the size in megabytes
 SIZE_IN_MB=$((SIZE/1024/1024))
+
+# note: to get drive info
+# wodim -prcap dev=/dev/cdrom
 
 echo "Writing $IMAGE_NAME ($SIZE_IN_MB MB) to drive ${DRIVE}"
 echo "Burning ISO $IMAGE ..."
 
-# create an image
-if ! wodim -eject -tao  speed=8 dev="${DRIVE}" -v -data "${IMAGE}"; then
+# burn image to disc
+# if ! wodim -eject -tao  speed=8 dev="${DRIVE}" -v -data "${IMAGE}"; then
+if ! growisofs -dvd-compat -Z "${DRIVE}"="${IMAGE}"; then
   echo >&2 "ERROR: Failed to burn image $IMAGE_NAME using drive ${DRIVE}."
   exit 1
 fi
 
-echo "Image $IMAGE_NAME burned to disk."
+# eject the disk
+echo "Ejecting ${DRIVE} ..."
+if ! eject "$DRIVE"; then
+  echo >&2 "WARNING: Failed to eject ${DRIVE}"
+fi
+
+echo "Image $IMAGE_NAME burned to disc."
 exit 0
